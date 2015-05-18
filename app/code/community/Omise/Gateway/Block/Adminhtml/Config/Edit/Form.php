eddaf67ce6bb148ac71940b26a04a17ca2407c3f
guzzilar <iam.nuttanon@gmail.com>
2015-05-18 17:51:46 +0700

    Reindent all of files from 2 space to 4 space

--- app/code/community/Omise/Gateway/Block/Adminhtml/Config/Edit/Form.php
+++ app/code/community/Omise/Gateway/Block/Adminhtml/Config/Edit/Form.php
@@ -1,82 +1,87 @@
 <?php
 class Omise_Gateway_Block_Adminhtml_Config_Edit_Form extends Mage_Adminhtml_Block_Widget_Form
 {
-  protected function _prepareForm()
-  {
-    // Instantiate a new form to display our brand for editing.
-    $form = new Varien_Data_Form(array( 'id'      => 'edit_form',
-                                        'action'  => $this->getUrl('#', array('_current' => true,
-                                                                              'continue' => 0)),
-                                        'method'  => 'post'));
-    $form->setUseContainer(true);
-    $this->setForm($form);
+    /**
+     *
+     * @return self
+     */
+    protected function _prepareForm()
+    {
+        // Instantiate a new form to display our brand for editing.
+        $form = new Varien_Data_Form(array( 'id'      => 'edit_form',
+                                            'action'  => $this->getUrl('#', array('_current' => true,
+                                                                                  'continue' => 0)),
+                                            'method'  => 'post'));
+        $form->setUseContainer(true);
+        $this->setForm($form);
 
-    $fieldset = $form->addFieldset('general', array('legend' => $this->__('Brand Details')));
+        $fieldset = $form->addFieldset('general', array('legend' => $this->__('Brand Details')));
 
-    $this->_addFieldsToFieldset($fieldset, array( 'public_key'  => array( 'label'     => $this->__('Public Key'),
-                                                                          'input'     => 'text',
-                                                                          'required'  => true),
+        $this->_addFieldsToFieldset($fieldset, array( 'public_key'  => array( 'label'     => $this->__('Public Key'),
+                                                                              'input'     => 'text',
+                                                                              'required'  => true),
 
-                                                  'secret_key'  => array( 'label'     => $this->__('Secret Key'),
-                                                                          'input'     => 'text',
-                                                                          'required'  => true)));
+                                                      'secret_key'  => array( 'label'     => $this->__('Secret Key'),
+                                                                              'input'     => 'text',
+                                                                              'required'  => true)));
 
-    return $this;
-  }
-
-  /**
-   * This method makes life a little easier for us by pre-populating
-   * fields with $_POST data where applicable and wrapping our post data
-   * in 'brandData' so that we can easily separate all relevant information
-   * in the controller. You could of course omit this method entirely
-   * and call the $fieldset->addField() method directly.
-   */
-  protected function _addFieldsToFieldset(Varien_Data_Form_Element_Fieldset $fieldset, $fields)
-  {
-      // $requestData = new Varien_Object($this->getRequest()
-      //   ->getPost('brandData'));
-
-    foreach ($fields as $name => $_data) {
-      // if ($requestValue = $requestData->getData($name)) {
-      //   $_data['value'] = $requestValue;
-      // }
-
-      //     // Wrap all fields with brandData group.
-      // $_data['name'] = "brandData[$name]";
-
-      //     // Generally, label and title are always the same.
-      // $_data['title'] = $_data['label'];
-
-      //     // If no new value exists, use the existing brand data.
-      // if (!array_key_exists('value', $_data)) {
-      //   $_data['value'] = $this->_getBrand()->getData($name);
-      // }
-
-      // Finally, call vanilla functionality to add field.
-      $fieldset->addField($name, $_data['input'], $_data);
+        return $this;
     }
 
-    return $this;
-  }
+    /**
+     * This method makes life a little easier for us by pre-populating
+     * fields with $_POST data where applicable and wrapping our post data
+     * in 'brandData' so that we can easily separate all relevant information
+     * in the controller. You could of course omit this method entirely
+     * and call the $fieldset->addField() method directly.
+     * @return self
+     */
+    protected function _addFieldsToFieldset(Varien_Data_Form_Element_Fieldset $fieldset, $fields)
+    {
+        // $requestData = new Varien_Object($this->getRequest()
+        //   ->getPost('brandData'));
 
-  /**
-   * Retrieve the existing brand for pre-populating the form fields.
-   * For a new brand entry, this will return an empty brand object.
-   */
-  protected function _getBrand()
-  {
-    if (!$this->hasData('brand')) {
-      // This will have been set in the controller.
-      $brand = Mage::registry('current_brand');
+        foreach ($fields as $name => $_data) {
+            // if ($requestValue = $requestData->getData($name)) {
+            //   $_data['value'] = $requestValue;
+            // }
 
-      // Just in case the controller does not register the brand.
-      if (!$brand instanceof SmashingMagazine_BrandDirectory_Model_Brand) {
-        $brand = Mage::getModel('smashingmagazine_branddirectory/brand');
-      }
+            //     // Wrap all fields with brandData group.
+            // $_data['name'] = "brandData[$name]";
 
-      $this->setData('brand', $brand);
+            //     // Generally, label and title are always the same.
+            // $_data['title'] = $_data['label'];
+
+            //     // If no new value exists, use the existing brand data.
+            // if (!array_key_exists('value', $_data)) {
+            //   $_data['value'] = $this->_getBrand()->getData($name);
+            // }
+
+            // Finally, call vanilla functionality to add field.
+            $fieldset->addField($name, $_data['input'], $_data);
+        }
+
+        return $this;
     }
 
-    return $this->getData('brand');
-  }
+    /**
+     * Retrieve the existing brand for pre-populating the form fields.
+     * For a new brand entry, this will return an empty brand object.
+     */
+    protected function _getBrand()
+    {
+        if (!$this->hasData('brand')) {
+            // This will have been set in the controller.
+            $brand = Mage::registry('current_brand');
+
+            // Just in case the controller does not register the brand.
+            if (!$brand instanceof SmashingMagazine_BrandDirectory_Model_Brand) {
+                $brand = Mage::getModel('smashingmagazine_branddirectory/brand');
+            }
+
+            $this->setData('brand', $brand);
+        }
+
+        return $this->getData('brand');
+    }
 }
--- app/code/community/Omise/Gateway/controllers/Adminhtml/OmiseController.php
+++ app/code/community/Omise/Gateway/controllers/Adminhtml/OmiseController.php
@@ -2,30 +2,42 @@
 
 class Omise_Gateway_Adminhtml_OmiseController extends Mage_Adminhtml_Controller_Action
 {
-  protected function _initAction()
-  {
-    // load layout, set active menu and breadcrumbs
-    $this->loadLayout()
-         ->_setActiveMenu('omise');
+    /**
+     *
+     * @return self
+     */
+    protected function _initAction()
+    {
+        // load layout, set active menu and breadcrumbs
+        $this->loadLayout()
+             ->_setActiveMenu('omise');
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function indexAction()
-  {
-    $this->_title($this->__('Index Action'))
-         ->_initAction()
-         ->renderLayout();
-  }
+    /**
+     *
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->_title($this->__('Index Action'))
+             ->_initAction()
+             ->renderLayout();
+    }
 
-  public function configAction()
-  {
-    $edit_block = $this->getLayout()
-                       ->createBlock('omise_gateway_adminhtml/config_edit');
+    /**
+     *
+     * @return void
+     */
+    public function configAction()
+    {
+        $edit_block = $this->getLayout()
+                           ->createBlock('omise_gateway_adminhtml/config_edit');
 
-    $this->_title($this->__('Config Action'))
-         ->_initAction()
-         ->_addContent($edit_block)
-         ->renderLayout();
-  }
+        $this->_title($this->__('Config Action'))
+             ->_initAction()
+             ->_addContent($edit_block)
+             ->renderLayout();
+    }
 }
