da7d62c7dfd5f93367050f2222144968200f66b0
guzzilar <iam.nuttanon@gmail.com>
2015-06-10 13:01:28 +0700

    Added authorize only feature when checkout an order

--- app/code/community/Omise/Gateway/Model/OmiseCharge.php
+++ app/code/community/Omise/Gateway/Model/OmiseCharge.php
@@ -15,4 +15,19 @@ class Omise_Gateway_Model_OmiseCharge extends Omise_Gateway_Model_Omise
             return array('error' => $e->getMessage());
         }
     }
+
+    /**
+     * Capture a charge that retrieve from charge id
+     * @param array $params
+     * @return OmiseCharge|Exception
+     */
+    public function captureOmiseCharge($id)
+    {
+        try {
+            $charge = OmiseCharge::retrieve($id, $this->_public_key, $this->_secret_key);
+            return $charge->capture();
+        } catch (Exception $e) {
+            return array('error' => $e->getMessage());
+        }
+    }
 }
--- app/code/community/Omise/Gateway/Model/PaymentMethod.php
+++ app/code/community/Omise/Gateway/Model/PaymentMethod.php
@@ -20,26 +20,63 @@ class Omise_Gateway_Model_PaymentMethod extends Mage_Payment_Model_Method_Cc
     protected $_canCapture      = true;
 
     /**
-     * Authorize payment abstract method
+     * Authorize payment method
      * @param Varien_Object $payment
      * @param float $amount
      * @return Mage_Payment_Model_Abstract
      */
     public function authorize(Varien_Object $payment, $amount)
     {
-        $omise = Mage::getModel('omise_gateway/omisecharge')->createOmiseCharge(array(
+        Mage::log('Start authorize with OmiseCharge API!');
+        
+        $charge = Mage::getModel('omise_gateway/omisecharge')->createOmiseCharge(array(
             "amount"        => number_format($amount, 2, '', ''),
             "currency"      => "thb",
             "description"   => 'xxx',
+            "capture"       => false,
             "card"          => $payment->getData('additional_information')['omise_token']));
 
-        if (isset($omise['error']))
-            Mage::throwException(Mage::helper('payment')->__('OmiseCharge:: '.$omise['error']));
+        if (isset($charge['error']))
+            Mage::throwException(Mage::helper('payment')->__('OmiseCharge:: '.$charge['error']));
 
-        // $payment->setIsTransactionClosed(false);
-        Mage::log('Authorize with OmiseCharge API!');
+        $this->getInfoInstance()->setAdditionalInformation('omise_charge_id', $charge['id']);
+
+        Mage::log('This transaction was authorized! (by OmiseCharge API)');
         return $this;
     }
+
+    /**
+     * Capture payment method
+     * @param Varien_Object $payment
+     * @param float $amount
+     * @return Mage_Payment_Model_Abstract
+     */
+    public function capture(Varien_Object $payment, $amount)
+    {
+        $authorized = isset($payment->getData('additional_information')['omise_charge_id']) ? $payment->getData('additional_information')['omise_charge_id'] : false;
+        if ($authorized) {
+            // Capture only.
+            Mage::log('Start capture with OmiseCharge API!');
+
+            $charge = Mage::getModel('omise_gateway/omisecharge')->captureOmiseCharge($authorized);
+        } else {
+            // Authorize and capture.
+            Mage::log('Start capture with OmiseCharge API!');
+
+            $charge = Mage::getModel('omise_gateway/omisecharge')->createOmiseCharge(array(
+                "amount"        => number_format($amount, 2, '', ''),
+                "currency"      => "thb",
+                "description"   => 'xxx',
+                "card"          => $payment->getData('additional_information')['omise_token']));
+        }
+
+        if (isset($charge['error']))
+            Mage::throwException(Mage::helper('payment')->__('OmiseCharge:: '.$charge['error']));
+
+        Mage::log('This transaction was authorized and captured! (by OmiseCharge API)');
+        return $this;
+    }
+
 
     /**
      * Assign data to info model instance
@@ -48,17 +85,21 @@ class Omise_Gateway_Model_PaymentMethod extends Mage_Payment_Model_Method_Cc
      */
     public function assignData($data)
     {
+        Mage::log('Assign Data with Omise');
+
         $result = parent::assignData($data);
 
         if (is_array($data)) {
             if (!isset($data['omise_token']))
                 Mage::throwException(Mage::helper('payment')->__('Need Omise\'s keys'));
 
+            Mage::log('Data that assign is Array');
             $this->getInfoInstance()->setAdditionalInformation('omise_token', $data['omise_token']);
         } elseif ($data instanceof Varien_Object) {
             if (!$data->getData('omise_token'))
                 Mage::throwException(Mage::helper('payment')->__('Need Omise\'s keys'));
 
+            Mage::log('Data that assign is Object');
             $this->getInfoInstance()->setAdditionalInformation('omise_token', $data->getData('omise_token'));
         }
 
