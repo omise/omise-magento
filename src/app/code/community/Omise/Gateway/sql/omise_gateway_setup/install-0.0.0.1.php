369203c963595c67e2785c7435915b9ba9aac0ff
Tanasak Janpan <bestnatade@gmail.com>
2015-08-05 17:06:51 +0700

    update sql installation

--- src/app/code/community/Omise/Gateway/sql/omise_gateway_setup/install-0.0.0.1.php
+++ src/app/code/community/Omise/Gateway/sql/omise_gateway_setup/install-0.0.0.1.php
@@ -2,6 +2,8 @@
 $this->startSetup();
 
 $table = new Varien_Db_Ddl_Table();
+
+//create omise_gateway/omise
 $table->setName($this->getTable('omise_gateway/omise'));
 
 $table->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array( 'identity'  => true,
@@ -19,5 +21,15 @@ $table->setOption('type', 'InnoDB');
 $table->setOption('charset', 'utf8');
 
 $this->getConnection()->createTable($table);
+
+//create omise_transaction/omise
+$table->setName($this->getTable('omise_transaction/omise'));
+$table->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array( 'identity'  => true,
+                                                                        'unsigned'  => true,
+                                                                        'nullable'  => false,
+                                                                        'primary'   => true));
+
+$table->addColumn('order_id', Varien_Db_Ddl_Table::TYPE_TEXT, 45, array('nullable' => false));
+$table->addColumn('transaction_id', Varien_Db_Ddl_Table::TYPE_TEXT, 45, array('nullable' => false));
 
 $this->endSetup();
