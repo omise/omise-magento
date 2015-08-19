4ab98558a4baa837e81d8a78d93b9cb0c2da6d15
Tanasak Janpan <bestnatade@gmail.com>
2015-08-19 17:18:58 +0700

    fixed database upgrade

--- src/app/code/community/Omise/Gateway/sql/omise_gateway_setup/upgrade-0.0.0.1-1.9.0.3.php
+++ src/app/code/community/Omise/Gateway/sql/omise_gateway_setup/upgrade-0.0.0.1-1.9.0.3.php
@@ -1,11 +1,11 @@
 <?php
-
 $this->startSetup();
 
 $table = new Varien_Db_Ddl_Table();
 
-//create omise_transaction/omise
-$table->setName($this->getTable('omise_transaction/omise'));
+//create omise_gateway/transaction
+$table->setName($this->getTable('omise_gateway/transaction'));
+
 $table->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array( 'identity'  => true,
                                                                         'unsigned'  => true,
                                                                         'nullable'  => false,
@@ -14,6 +14,8 @@ $table->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array( 'identit
 $table->addColumn('order_id', Varien_Db_Ddl_Table::TYPE_TEXT, 45, array('nullable' => false));
 $table->addColumn('transaction_id', Varien_Db_Ddl_Table::TYPE_TEXT, 45, array('nullable' => false));
 
+$table->setOption('type', 'InnoDB');
+$table->setOption('charset', 'utf8');
 
 $this->getConnection()->createTable($table);
 
