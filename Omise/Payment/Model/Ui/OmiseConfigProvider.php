c016a5e96b8fffa6c06832d88f419ff278fe22e2
Saroj Saengphongumphai <ss.saroj@gmail.com>
2016-10-25 11:19:28 +0700

    Display the list of month at the card expiration month dropdown

---
+++ Omise/Payment/Model/Ui/OmiseConfigProvider.php
@@ -0,0 +1,33 @@
+<?php
+namespace Omise\Payment\Model\Ui;
+
+use Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\Payment\Model\CcConfig;
+
+class OmiseConfigProvider implements ConfigProviderInterface
+{
+    const CODE = 'omise';
+
+    protected $ccConfig;
+
+    public function __construct(CcConfig $ccConfig)
+    {
+        $this->ccConfig = $ccConfig;
+    }
+
+    public function getCcMonths()
+    {
+        return $this->ccConfig->getCcMonths();
+    }
+
+    public function getConfig()
+    {
+        return [
+            'payment' => [
+                'ccform' => [
+                    'months' => [self::CODE => $this->getCcMonths()]
+                ],
+            ]
+        ];
+    }
+}
--- Omise/Payment/view/frontend/web/js/view/payment/method-renderer/omise-cc-method.js
+++ Omise/Payment/view/frontend/web/js/view/payment/method-renderer/omise-cc-method.js
@@ -1,16 +1,12 @@
 define(
     [
-        'Magento_Checkout/js/view/payment/default'
+        'Magento_Payment/js/view/payment/cc-form'
     ],
     function (Component) {
         'use strict';
         return Component.extend({
             defaults: {
                 template: 'Omise_Payment/payment/omise-cc-form'
-            },
-
-            getMailingAddress: function() {
-                return window.checkoutConfig.payment.checkmo.mailingAddress;
             },
         });
     }
