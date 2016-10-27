5212b994cff067dcc30c297df02793982271ed11
Saroj Saengphongumphai <ss.saroj@gmail.com>
2016-10-27 18:01:51 +0700

    Create Omise card token at the client side script
    The checkout form has not been sending to the server side.

--- Omise/Payment/view/frontend/web/js/view/payment/method-renderer/omise-cc-method.js
+++ Omise/Payment/view/frontend/web/js/view/payment/method-renderer/omise-cc-method.js
@@ -1,8 +1,9 @@
 define(
     [
-        'Magento_Payment/js/view/payment/cc-form'
+        'Magento_Payment/js/view/payment/cc-form',
+        'mage/translate',
     ],
-    function (Component) {
+    function (Component, $t) {
         'use strict';
 
         return Component.extend({
@@ -19,6 +20,24 @@ define(
                 return 'omise';
             },
 
+            getPublicKey: function() {
+                return window.checkoutConfig.payment.omise.publicKey;
+            },
+
+            initObservable: function() {
+                this._super()
+                    .observe([
+                        'omiseCardNumber',
+                        'omiseCardHolderName',
+                        'omiseCardExpirationMonth',
+                        'omiseCardExpirationYear',
+                        'omiseCardSecurityCode',
+                        'omiseCardToken'
+                    ]);
+
+                return this;
+            },
+
             /**
              * Is method available to display
              *
@@ -26,7 +45,35 @@ define(
              */
             isActive: function() {
                 return true;
-            }
+            },
+
+            placeOrder: function(data, event) {
+                var self = this;
+
+                if (typeof Omise === 'undefined') {
+                    alert($t('Unable to process the payment, loading the external card processing library is failed. Please contact the merchant.'));
+                    return false;
+                }
+
+                var card = {
+                    number: this.omiseCardNumber(),
+                    name: this.omiseCardHolderName(),
+                    expiration_month: this.omiseCardExpirationMonth(),
+                    expiration_year: this.omiseCardExpirationYear(),
+                    security_code: this.omiseCardSecurityCode()
+                };
+
+                Omise.setPublicKey(this.getPublicKey());
+                Omise.createToken('card', card, function(statusCode, response) {
+                    if (statusCode === 200) {
+                        self.omiseCardToken(response.id);
+                    } else {
+                        alert(response.message);
+                    }
+                });
+
+                return false;
+            },
         });
     }
 );
