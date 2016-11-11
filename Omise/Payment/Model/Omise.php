6d85eb2d6ed415c50efe3546e62c435b49ccb518
nam <nam@omise.co>
2016-11-11 12:09:34 +0700

    Update PHPCodeComment block.

--- Omise/Payment/Model/Omise.php
+++ Omise/Payment/Model/Omise.php
@@ -9,14 +9,54 @@ class Omise extends Cc
 {
     const CODE = 'omise';
 
-    protected $_canAuthorize = true;
-    protected $_canCapture = true;
+    /**
+     * @var string
+     */
     protected $_code = 'omise';
+
+    /**
+     * Payment Method feature
+     *
+     * @var bool
+     */
     protected $_isGateway = true;
 
+    /**
+     * Payment Method feature
+     *
+     * @var bool
+     */
+    protected $_canAuthorize = true;
+
+    /**
+     * Payment Method feature
+     *
+     * @var bool
+     */
+    protected $_canCapture = true;
+
+    /**
+     * Authorize payment abstract method
+     *
+     * @api
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     *
+     * @param  \Magento\Framework\DataObject|InfoInterface $payment
+     * @param  float $amount
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     *
+     * @return $this
+     */
     public function authorize(InfoInterface $payment, $amount)
     {
+        if (!$this->canAuthorize()) {
+            throw new \Magento\Framework\Exception\LocalizedException(__('The authorize action is not available.'));
+        }
+
         $payment->setAdditionalInformation('payment_type', $this->getConfigData('payment_action'));
+
+        return $this;
     }
 
     public function isAvailable(CartInterface $quote = null)
