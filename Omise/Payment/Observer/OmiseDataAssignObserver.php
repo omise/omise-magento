80451a5035f0432b9d5d006bc1df5a4371c7a003
Saroj Saengphongumphai <ss.saroj@gmail.com>
2016-11-16 15:21:07 +0700

    Add and declare an observer class to catch the parameter from client
    Add an observer class, OmiseDataAssignObserver, and declare it in
    events.xml. This observer class will catch the parameter from client
    side and assign it to use in the next process.

---
+++ Omise/Payment/Observer/OmiseDataAssignObserver.php
@@ -0,0 +1,39 @@
+<?php
+namespace Omise\Payment\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Payment\Observer\AbstractDataAssignObserver;
+use Magento\Quote\Api\Data\PaymentInterface;
+
+class OmiseDataAssignObserver extends AbstractDataAssignObserver
+{
+    const OMISE_CARD_TOKEN = 'omise_card_token';
+
+    protected $additionalInformationList = [
+        self::OMISE_CARD_TOKEN
+    ];
+
+    public function execute(Observer $observer)
+    {
+        $dataObject = $this->readDataArgument($observer);
+
+        $additionalData = $dataObject->getData(PaymentInterface::KEY_ADDITIONAL_DATA);
+
+        if (!is_array($additionalData)) {
+            return;
+        }
+
+        $paymentInfo = $this->readPaymentModelArgument($observer);
+
+        $paymentInfo->setOmiseCardToken($additionalData[self::OMISE_CARD_TOKEN]);
+
+        foreach ($this->additionalInformationList as $additionalInformationKey) {
+            if (isset($additionalData[$additionalInformationKey])) {
+                $paymentInfo->setAdditionalInformation(
+                    $additionalInformationKey,
+                    $additionalData[$additionalInformationKey]
+                );
+            }
+        }
+    }
+}
