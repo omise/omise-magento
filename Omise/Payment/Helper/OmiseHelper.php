9ddc29d71dea19cd1dd2d1f715fc96a038f3c576
nam <nam@omise.co>
2016-11-18 04:04:45 +0700

    Add omiseAmountFormat helper function.
    To convert the Magento order amount to Omise amount format

--- Omise/Payment/Helper/OmiseHelper.php
+++ Omise/Payment/Helper/OmiseHelper.php
@@ -17,4 +17,28 @@ class OmiseHelper extends AbstractHelper
 
         return $this->scopeConfig->getValue($path, ScopeInterface::SCOPE_STORE);
     }
+
+    /**
+     * @param  string  $currency
+     * @param  integer $amount
+     *
+     * @return string
+     */
+    public function omiseAmountFormat($currency, $amount)
+    {
+        switch (strtoupper($currency)) {
+            case 'THB':
+                // Convert to satang unit
+                $amount = $amount * 100;
+                break;
+
+            case 'JPY':
+                break;
+
+            default:
+                break;
+        }
+
+        return $amount;
+    }
 }
