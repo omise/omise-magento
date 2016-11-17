984fcc56c6756bc3a5a831308fd35eedcc2993e8
nam <nam@omise.co>
2016-11-18 04:10:02 +0700

    Attach the amount and currency params at the PaymentDataBuilder class.
    To send the proper parameters (amount, currency, omise_card_token) to the Gateway\Http\Client classes.
    We have to build a requested body by ourself through PaymentDataBuilder class.

--- Omise/Payment/Gateway/Request/PaymentDataBuilder.php
+++ Omise/Payment/Gateway/Request/PaymentDataBuilder.php
@@ -3,17 +3,49 @@ namespace Omise\Payment\Gateway\Request;
 
 use Magento\Payment\Gateway\Helper\SubjectReader;
 use Magento\Payment\Gateway\Request\BuilderInterface;
+use Omise\Payment\Helper\OmiseHelper;
 use Omise\Payment\Observer\OmiseDataAssignObserver;
 
 class PaymentDataBuilder implements BuilderInterface
 {
+    /**
+     * @var string
+     */
+    const OMISE_TOKEN = 'omise_card_token';
 
+    /**
+     * @var string
+     */
+    const AMOUNT = 'amount';
+
+    /**
+     * @var string
+     */
+    const CURRENCY = 'currency';
+
+    /**
+     * @param \Omise\Payment\Helper\OmiseHelper $omiseHelper
+     */
+    public function __construct(OmiseHelper $omiseHelper)
+    {
+        $this->omiseHelper = $omiseHelper;
+    }
+
+    /**
+     * @param  array $buildSubject
+     *
+     * @return array
+     */
     public function build(array $buildSubject)
     {
-        $payment = SubjectReader::readPayment($buildSubject)->getPayment();
+        $payment = SubjectReader::readPayment($buildSubject);
+        $method  = $payment->getPayment();
+        $order   = $payment->getOrder();
 
         return [
-            'omise_card_token' => $payment->getAdditionalInformation(OmiseDataAssignObserver::OMISE_CARD_TOKEN)
+            self::AMOUNT      => $this->omiseHelper->omiseAmountFormat($order->getCurrencyCode(), $order->getGrandTotalAmount()),
+            self::CURRENCY    => $order->getCurrencyCode(),
+            self::OMISE_TOKEN => $method->getAdditionalInformation(OmiseDataAssignObserver::OMISE_CARD_TOKEN)
         ];
     }
 }
