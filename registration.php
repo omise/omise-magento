434c5a37b4dad81bb7cdf7a9d8e4e3d40fff8115
guzzilar <iam.nuttanon@gmail.com>
2016-12-20 20:33:19 +0700

    Remove a line that require Gateway/Http/Lib/omise-php lib

--- registration.php
+++ registration.php
@@ -1,6 +1,4 @@
 <?php
 use \Magento\Framework\Component\ComponentRegistrar;
 
-require_once dirname(__FILE__) . '/Gateway/Http/Lib/omise-php/lib/Omise.php';
-
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Omise_Payment', __DIR__);
