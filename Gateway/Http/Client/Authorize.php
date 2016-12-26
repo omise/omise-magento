7252f1ca1c217b4df806ff08ce2dcb568e4ee25d
guzzilar <iam.nuttanon@gmail.com>
2016-12-27 05:48:04 +0700

    Implement conditional block to check add -return_uri- param to the Authorize command

--- Gateway/Http/Client/Authorize.php
+++ Gateway/Http/Client/Authorize.php
@@ -2,6 +2,7 @@
 namespace Omise\Payment\Gateway\Http\Client;
 
 use Omise\Payment\Gateway\Request\PaymentDataBuilder;
+use Omise\Payment\Gateway\Request\ThreeDSecureDataBuilder;
 
 class Authorize extends AbstractOmiseClient
 {
@@ -12,16 +13,18 @@ class Authorize extends AbstractOmiseClient
      */
     public function request(Array $body)
     {
-        return \OmiseCharge::create(
-            [
-                'amount'      => $body[PaymentDataBuilder::AMOUNT],
-                'currency'    => $body[PaymentDataBuilder::CURRENCY],
-                'card'        => $body[PaymentDataBuilder::OMISE_TOKEN],
-                'capture'     => false,
-                'description' => $body[PaymentDataBuilder::ORDER_ID],
-            ],
-            $this->publicKey,
-            $this->secretKey
-        );
+        $params = [
+            'amount'      => $body[PaymentDataBuilder::AMOUNT],
+            'currency'    => $body[PaymentDataBuilder::CURRENCY],
+            'card'        => $body[PaymentDataBuilder::OMISE_TOKEN],
+            'capture'     => false,
+            'description' => $body[PaymentDataBuilder::ORDER_ID],
+        ];
+
+        if ($body[ThreeDSecureDataBuilder::REQUIRE_REDIRECT]) {
+            $params['return_uri'] = $body[ThreeDSecureDataBuilder::RETURN_URI];
+        }
+
+        return \OmiseCharge::create($params, $this->publicKey, $this->secretKey);
     }
 }
