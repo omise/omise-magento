d10533820971be4def8db512c3680fd248b792bf
guzzilar <iam.nuttanon@gmail.com>
2017-01-01 21:47:46 +0700

    Finish ThreeDSecureDataBuilder class/functions. Refactor code, change constant name, add docblock.

--- Gateway/Request/ThreeDSecureDataBuilder.php
+++ Gateway/Request/ThreeDSecureDataBuilder.php
@@ -10,12 +10,17 @@ class ThreeDSecureDataBuilder implements BuilderInterface
     /**
      * @var string
      */
-    const RETURN_URI = 'return_uri';
+    const PROCESS_3DSECURE = 'process_3dsecure';
 
     /**
      * @var string
      */
-    const REQUIRE_REDIRECT = 'require_redirect';
+    const RETURN_URI = 'return_uri';
+
+    /**
+     * @var \Omise\Payment\Helper\OmiseHelper
+     */
+    protected $omiseHelper;
 
     /**
      * @param \Omise\Payment\Helper\OmiseHelper $omiseHelper
@@ -26,6 +31,8 @@ class ThreeDSecureDataBuilder implements BuilderInterface
     }
 
     /**
+     * Build request parameters
+     *
      * @param  array $buildSubject
      *
      * @return array
@@ -33,27 +40,56 @@ class ThreeDSecureDataBuilder implements BuilderInterface
     public function build(array $buildSubject)
     {
         if ($this->is3DSecureEnabled()) {
-            return [
-                self::RETURN_URI       => $this->omiseHelper->getUrl('/'),
-                self::REQUIRE_REDIRECT => true
-            ];
+            return $this->buildRequestWith3DSecure($buildSubject);
         }
 
-        return [
-            self::RETURN_URI       => "",
-            self::REQUIRE_REDIRECT => false
-        ];
+        return $this->buildRequestWithout3DSecure($buildSubject);
     }
 
     /**
+     * Check whether 3-D Secure config enable or not.
+     *
      * @return boolean
      */
     protected function is3DSecureEnabled()
     {
-        if (! $this->omiseHelper->getConfig('3ds')) {
-            return false;
+        if ($this->omiseHelper->getConfig('3ds')) {
+            return true;
         }
 
-        return true;
+        return false;
+    }
+
+    /**
+     * Build request parameters with 3-D Secure process required.
+     *
+     * @param  array $buildSubject
+     *
+     * @return array
+     */
+    protected function buildRequestWith3DSecure(array $buildSubject)
+    {
+        $payment = SubjectReader::readPayment($buildSubject);
+        $order   = $payment->getOrder();
+
+        return [
+            self::PROCESS_3DSECURE => true,
+            self::RETURN_URI       => $this->omiseHelper->getUrl('omise/omise/callback', ['_secure' => true])
+        ];
+    }
+
+    /**
+     * Build request parameters without 3-D Secure process required.
+     *
+     * @param  array $buildSubject
+     *
+     * @return array
+     */
+    protected function buildRequestWithout3DSecure(array $buildSubject)
+    {
+        return [
+            self::PROCESS_3DSECURE => false,
+            self::RETURN_URI       => ""
+        ];
     }
 }
