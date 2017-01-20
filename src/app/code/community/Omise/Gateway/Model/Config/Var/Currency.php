b30926c9c95d1b7ed6ec94b2befb530f71281fdc
guzzilar <iam.nuttanon@gmail.com>
2017-01-20 12:22:41 +0700

    Model/Config/Var/Currency.php:: Code styling

--- src/app/code/community/Omise/Gateway/Model/Config/Var/Currency.php
+++ src/app/code/community/Omise/Gateway/Model/Config/Var/Currency.php
@@ -3,6 +3,7 @@ class Omise_Gateway_Model_Config_Var_Currency
 {
     /**
      * Return an array that use for 'currency' configuration
+     *
      * @return array
      */
     public function toOptionArray()
