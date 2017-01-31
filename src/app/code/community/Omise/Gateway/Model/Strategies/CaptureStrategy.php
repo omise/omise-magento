d28dc8ac259dfe19a71f30e6a796149191a34448
guzzilar <iam.nuttanon@gmail.com>
2017-02-01 06:01:29 +0700

    Implement payment capture strategy.

---
+++ src/app/code/community/Omise/Gateway/Model/Strategies/CaptureStrategy.php
@@ -0,0 +1,57 @@
+<?php
+class Omise_Gateway_Model_Strategies_CaptureStrategy extends Omise_Gateway_Model_Strategies_StrategyAbstract
+{
+    /**
+     * Process a payment.
+     *
+     * @param  array|mixed $params
+     *
+     * @return mixed
+     */
+    public function process($params = array())
+    {
+        if (isset($params['id'])) {
+            $charge = OmiseCharge::retrieve($params['id']);
+            return $charge->capture();
+        }
+
+        return OmiseCharge::create($params);
+    }
+
+    /**
+     * Validate a payment process result.
+     *
+     * @param  \OmiseCharge $charge
+     *
+     * @return boolean
+     *
+     * @see    https://github.com/omise/omise-php/blob/master/lib/omise/OmiseCharge.php
+     */
+    public function validate($charge)
+    {
+        if (! isset($charge['object'])) {
+            $this->message = 'Cannot retrieve a payment result, please contact our support to confirm the payment.';
+            return false;
+        }
+
+        if ($charge['object'] === 'error') {
+            $this->message = $charge['message'];
+            return false;
+        }
+
+        if (! $charge['authorized'] || ! $charge['captured']) {
+            $this->message = 'Your payment failed:: (' . $charge['failure_code'] . ') - ' . $charge['failure_code'];
+            return false;
+        }
+
+        if ($charge['object'] === 'charge'
+            && $charge['status'] === 'successful'
+            && $charge['authorized'] === true
+            && $charge['captured'] === true) {
+            return true;
+        }
+
+        $this->message = 'Error payment result validation, please contact our support to confirm the payment.';
+        return false;
+    }
+}
