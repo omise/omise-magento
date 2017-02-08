a2b34e0e1c884513147979b6d0f1d4732ea5f1d9
guzzilar <iam.nuttanon@gmail.com>
2017-02-08 15:11:43 +0700

    Add AuthorizeThreeDSecureStrategy & CaptureThreeDSecureStrategy classes

---
+++ src/app/code/community/Omise/Gateway/Model/Strategies/CaptureThreeDSecureStrategy.php
@@ -0,0 +1,25 @@
+<?php
+class Omise_Gateway_Model_Strategies_CaptureThreeDSecureStrategy extends Omise_Gateway_Model_Strategies_StrategyAbstract
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function perform($payment, $amount)
+    {
+        // ...
+    }
+
+    /**
+     * Validate a payment process result.
+     *
+     * @param  \OmiseCharge $charge
+     *
+     * @return boolean
+     *
+     * @see    https://github.com/omise/omise-php/blob/master/lib/omise/OmiseCharge.php
+     */
+    public function validate($charge)
+    {
+        // ...
+    }
+}
