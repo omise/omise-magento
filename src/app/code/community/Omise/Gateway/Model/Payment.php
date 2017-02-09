bad8251beb19a988887d9535741a1a35aa7383b5
guzzilar <iam.nuttanon@gmail.com>
2017-02-09 15:48:09 +0700

    Separate between class that taking care of configuration and class that taking care of payment action.
    OmiseConfiguration ===== OmiseModule ===== OmisePayment
                                                    |
                                        ----------------------------
                                        |                          |
                                        CreditCardPayment          OffsiteInternetBankingPayment
    
    OmiseConfiguration (Model/Config.php) - Take care of retrieve data from Magento environment (i.e. DB)
    OmiseModule (Model/Omise.php)         - Take care of core part that need to meet with Omise API requirement.
    OmisePayment (Model/Payment.php)      - Base class for handle Magento's payment gateway action (i.e. authorize(), capture(), refund(), void(), etc).

--- src/app/code/community/Omise/Gateway/Model/Payment.php
+++ src/app/code/community/Omise/Gateway/Model/Payment.php
@@ -7,18 +7,9 @@ abstract class Omise_Gateway_Model_Payment extends Mage_Payment_Model_Method_Abs
     protected $config;
 
     /**
-     * Omise's public key
-     *
-     * @var string
+     * @var \Omise_Gateway_Model_Omise
      */
-    protected $public_key;
-
-    /**
-     * Omise's secret key
-     *
-     * @var string
-     */
-    protected $secret_key;
+    protected $omise;
 
     /**
      * @var \Mage_Sales_Model_Order_Payment
@@ -32,26 +23,8 @@ abstract class Omise_Gateway_Model_Payment extends Mage_Payment_Model_Method_Abs
      */
     public function __construct()
     {
-        // Load Omise-PHP library
-        require_once(Mage::getBaseDir('lib') . '/omise-php/lib/Omise.php');
-
         $this->config = Mage::getModel('omise_gateway/config')->load(1);
-
-        if ($this->isTestMode()) {
-            $this->public_key = $this->config->public_key_test;
-            $this->secret_key = $this->config->secret_key_test;
-        } else {
-            $this->public_key = $this->config->public_key;
-            $this->secret_key = $this->config->secret_key;
-        }
-    }
-
-    /**
-     * @return bool
-     */
-    public function isTestMode()
-    {
-        return $this->config->test_mode;
+        $this->omise  = Mage::getModel('omise_gateway/omise');
     }
 
     /**
@@ -74,9 +47,9 @@ abstract class Omise_Gateway_Model_Payment extends Mage_Payment_Model_Method_Abs
         Mage_Sales_Model_Order_Payment $payment,
         $amount
     ) {
-        $this->defineOmiseKeys();
-        $this->defineOmiseApiVersion();
-        $this->defineOmiseUserAgent();
+        $this->omise->defineApiKeys();
+        $this->omise->defineApiVersion();
+        $this->omise->defineUserAgent();
 
         $this->payment_information = $payment;
 
@@ -91,44 +64,5 @@ abstract class Omise_Gateway_Model_Payment extends Mage_Payment_Model_Method_Abs
         }    
 
         return $result;
-    }
-
-    /**
-     * @param  string $public_key
-     * @param  string $secret_key
-     *
-     * @return void
-     */
-    protected function defineOmiseKeys($public_key = '', $secret_key = '')
-    {
-        if (! defined('OMISE_PUBLIC_KEY')) {
-            define('OMISE_PUBLIC_KEY', $public_key ? $public_key : $this->public_key);
-        }
-
-        if (! defined('OMISE_SECRET_KEY')) {
-            define('OMISE_SECRET_KEY', $secret_key ? $secret_key : $this->secret_key);
-        }
-    }
-
-    /**
-     * @param  string $version
-     *
-     * @return void
-     */
-    protected function defineOmiseApiVersion($version = '2014-07-27')
-    {
-        if (! defined('OMISE_API_VERSION')) {
-            define('OMISE_API_VERSION', $version);
-        }
-    }
-
-    /**
-     * @return void
-     */
-    protected function defineOmiseUserAgent()
-    {
-        if (! defined('OMISE_USER_AGENT_SUFFIX')) {
-            define('OMISE_USER_AGENT_SUFFIX', 'OmiseMagento/1.9.0.6 Magento/' . Mage::getVersion());
-        }
     }
 }
