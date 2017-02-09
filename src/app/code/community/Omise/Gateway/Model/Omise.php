3a8b146095137597d242266e25c686c983413444
guzzilar <iam.nuttanon@gmail.com>
2017-02-09 15:48:09 +0700

    Separate between class that taking care of configuration and class that taking care of payment action.
    OmiseConfiguration ===== OmiseModule ===== OmisePayment
                                                    |
                                        ----------------------------
                                        |                          |
                                        CreditCardPayment          OffsiteInternetBankingPayment
    
    OmiseConfiguration (Model/Config.php) - Take care of retrieve data from Magento environment (i.e. DB)
    OmiseModule (Model/Omise.php)         - Take care of core part that need to meet with Omise API requirement.
    OmisePayment (Model/Payment.php)      - Base class for handle Magento's payment gateway action (i.e. authorize(), capture(), refund(), void(), etc).

--- src/app/code/community/Omise/Gateway/Model/Omise.php
+++ src/app/code/community/Omise/Gateway/Model/Omise.php
@@ -1,25 +1,24 @@
 <?php
-// Define 'OMISE_USER_AGENT_SUFFIX'
-if (! defined('OMISE_USER_AGENT_SUFFIX')) {
-    define('OMISE_USER_AGENT_SUFFIX', 'OmiseMagento/1.9.0.6 Magento/' . Mage::getVersion());
-}
-
-// Define 'OMISE_API_VERSION'
-if (! defined('OMISE_API_VERSION')) {
-    define('OMISE_API_VERSION', '2014-07-27');
-}
-
 class Omise_Gateway_Model_Omise extends Mage_Core_Model_Abstract
 {
     /**
+     * @deprecated use $this->getPublicKey(); instead.
+     *
      * @var string  Omise public key
      */
     protected $_public_key;
 
     /**
+     * @deprecated use $this->getSecretKey(); instead.
+     *
      * @var string  Omise secret key
      */
     protected $_secret_key;
+
+    /**
+     * @var \Omise_Gateway_Model_Config
+     */
+    protected $config;
 
     /**
      * Load necessary file and setup Omise keys
@@ -32,14 +31,91 @@ class Omise_Gateway_Model_Omise extends Mage_Core_Model_Abstract
         require_once(Mage::getBaseDir('lib') . '/omise-php/lib/Omise.php');
 
         // Retrieve Omise's keys from table.
-        $omise = Mage::getModel('omise_gateway/config')->load(1);
-        $this->_public_key = $omise->public_key;
-        $this->_secret_key = $omise->secret_key;
+        $this->config = Mage::getModel('omise_gateway/config')->load(1);
 
-        // Replace keys with test keys if test mode was enabled.
-        if ($omise->test_mode) {
-            $this->_public_key = $omise->public_key_test;
-            $this->_secret_key = $omise->secret_key_test;
+        // @deprecated use $this->getPublicKey() and $this->getSecretKey() instead.
+        $this->_public_key = $this->getPublicKey();
+        $this->_secret_key = $this->getSecretKey();
+    }
+
+    /**
+     * @return bool
+     */
+    public function isTestMode()
+    {
+        return $this->config->test_mode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPublicKey()
+    {
+        if ($this->isTestMode()) {
+            return $this->config->public_key_test;
         }
+
+        return $this->config->public_key;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSecretKey()
+    {
+        if ($this->isTestMode()) {
+            return $this->config->secret_key_test;
+        }
+
+        return $this->config->secret_key;
+    }
+
+    /**
+     * @param  string $public_key
+     * @param  string $secret_key
+     *
+     * @return void
+     */
+    public function defineApiKeys($public_key = '', $secret_key = '')
+    {
+        if (! defined('OMISE_PUBLIC_KEY')) {
+            define('OMISE_PUBLIC_KEY', $public_key ? $public_key : $this->getPublicKey());
+        }
+
+        if (! defined('OMISE_SECRET_KEY')) {
+            define('OMISE_SECRET_KEY', $secret_key ? $secret_key : $this->getSecretKey());
+        }
+    }
+
+    /**
+     * @param  string $version
+     *
+     * @return void
+     */
+    public function defineApiVersion($version = '2014-07-27')
+    {
+        if (! defined('OMISE_API_VERSION')) {
+            define('OMISE_API_VERSION', $version);
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function defineUserAgent()
+    {
+        if (! defined('OMISE_USER_AGENT_SUFFIX')) {
+            define('OMISE_USER_AGENT_SUFFIX', 'OmiseMagento/1.9.0.6 Magento/' . Mage::getVersion());
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function initNecessaryConstant()
+    {
+        $this->defineApiKeys();
+        $this->defineApiVersion();
+        $this->defineUserAgent();
     }
 }
