a4191bf96dc2e693a0a5e990ef69984244137569
guzzilar <iam.nuttanon@gmail.com>
2017-03-24 17:18:20 +0700

    Add minimum required files to display an Internet Banking payment method.

---
+++ Model/Config/Offsite/Internetbanking.php
@@ -0,0 +1,12 @@
+<?php
+namespace Omise\Payment\Model\Config\Offsite;
+
+use Omise\Payment\Model\Config\Config;
+
+class Internetbanking extends Config
+{
+    /**
+     * @var string
+     */
+    const CODE = 'omise_offsite_internetbanking';
+}
