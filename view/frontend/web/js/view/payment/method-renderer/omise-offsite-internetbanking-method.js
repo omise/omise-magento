3ac4e2f1a44337db085954488290e156413ef6c8
guzzilar <iam.nuttanon@gmail.com>
2017-03-31 18:09:08 +0700

    Handle place order action, either redirect buyer out if success or raise an error message if it fail.

--- view/frontend/web/js/view/payment/method-renderer/omise-offsite-internetbanking-method.js
+++ view/frontend/web/js/view/payment/method-renderer/omise-offsite-internetbanking-method.js
@@ -1,15 +1,25 @@
 define(
     [
+        'jquery',
         'ko',
+        'mage/storage',
         'Magento_Checkout/js/view/payment/default',
+        'Magento_Checkout/js/model/full-screen-loader',
         'Magento_Checkout/js/action/redirect-on-success',
-        'Magento_Checkout/js/model/quote'
+        'Magento_Checkout/js/model/quote',
+        'Magento_Checkout/js/model/url-builder',
+        'Magento_Checkout/js/model/error-processor'
     ],
     function (
+        $,
         ko,
+        storage,
         Component,
+        fullScreenLoader,
         redirectOnSuccessAction,
-        quote
+        quote,
+        urlBuilder,
+        errorProcessor
     ) {
         'use strict';
 
@@ -81,14 +91,41 @@ define(
 
                 self.getPlaceOrderDeferredObject()
                     .fail(
-                        function() {
-                            console.log('failed');
+                        function(response) {
+                            errorProcessor.process(response, self.messageContainer);
+                            fullScreenLoader.stopLoader();
+                            self.isPlaceOrderActionAllowed(true);
                         }
                     ).done(
-                        function() {
-                            self.afterPlaceOrder();
+                        function(response) {
+                            var self = this;
 
-                            redirectOnSuccessAction.execute();
+                            var serviceUrl = urlBuilder.createUrl(
+                                '/orders/:order_id/omise-offsite',
+                                {
+                                    order_id: response
+                                }
+                            );
+
+                            storage.get(serviceUrl, false)
+                                .fail(
+                                    function (response) {
+                                        errorProcessor.process(response, self.messageContainer);
+                                        fullScreenLoader.stopLoader();
+                                        self.isPlaceOrderActionAllowed(true);
+                                    }
+                                )
+                                .done(
+                                    function (response) {
+                                        if (response) {
+                                            $.mage.redirect(response.authorize_uri);
+                                        } else {
+                                            errorProcessor.process(response, self.messageContainer);
+                                            fullScreenLoader.stopLoader();
+                                            self.isPlaceOrderActionAllowed(true);
+                                        }
+                                    }
+                                );
                         }
                     );
 
