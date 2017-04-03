226bba1519feebced364df9916d5774e82dd2f89
guzzilar <iam.nuttanon@gmail.com>
2017-04-03 13:43:52 +0700

    Rename all of related files according to the changes in its filename.

--- src/app/code/community/Omise/Gateway/controllers/Callback/ValidatethreedsecureController.php
+++ src/app/code/community/Omise/Gateway/controllers/Callback/ValidatethreedsecureController.php
@@ -1,5 +1,5 @@
 <?php
-class Omise_Gateway_Callback_ValidateThreeDSecureController extends Mage_Core_Controller_Front_Action
+class Omise_Gateway_Callback_ValidatethreedsecureController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
