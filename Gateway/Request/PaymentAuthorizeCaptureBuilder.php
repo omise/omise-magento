4e54da5961d92ceffccd20fcdff321824c919e75
guzzilar <iam.nuttanon@gmail.com>
2017-04-07 23:59:38 +0700

    Add request builders to handle 'authorie', 'authorize and capture' behaviours.

---
+++ Gateway/Request/PaymentAuthorizeCaptureBuilder.php
@@ -0,0 +1,22 @@
+<?php
+namespace Omise\Payment\Gateway\Request;
+
+use Magento\Payment\Gateway\Request\BuilderInterface;
+
+class PaymentAuthorizeCaptureBuilder implements BuilderInterface
+{
+    /**
+     * @var string
+     */
+    const CAPTURE = 'capture';
+
+    /**
+     * @param  array $buildSubject
+     *
+     * @return array
+     */
+    public function build(array $buildSubject)
+    {
+        return [ self::CAPTURE => true ];
+    }
+}
