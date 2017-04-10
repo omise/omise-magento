300913ac80e5e536486e8d1618e4049cb96127f1
guzzilar <iam.nuttanon@gmail.com>
2017-04-11 00:15:43 +0700

    Validate a 3-D Secure payment result at the callback uri.

--- Gateway/Validator/OmiseAuthorizeCommandResponseValidator.php
+++ Gateway/Validator/OmiseAuthorizeCommandResponseValidator.php
@@ -12,7 +12,7 @@ class OmiseAuthorizeCommandResponseValidator extends CommandResponseValidator
      *
      * @return mixed
      */
-    protected function validateResponse($data)
+    public function validateResponse($data)
     {
         if (! isset($data['object']) || $data['object'] !== 'charge') {
             return new OmiseObjectInvalid();
--- Gateway/Validator/ThreeDSecureCommandResponseValidator.php
+++ Gateway/Validator/ThreeDSecureCommandResponseValidator.php
@@ -12,7 +12,7 @@ class ThreeDSecureCommandResponseValidator extends CommandResponseValidator
      *
      * @return mixed
      */
-    protected function validateResponse($data)
+    public function validateResponse($data)
     {
         if (! isset($data['object']) || $data['object'] !== 'charge') {
             return new OmiseObjectInvalid();
