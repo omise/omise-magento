805dc24ae13051cd0b59b682f00f2cf8dfc24592
guzzilar <iam.nuttanon@gmail.com>
2017-10-20 01:18:13 +0700

    Add PHPDoc for internal constructor methods (_constructor(), only one underscore).

--- src/app/code/community/Omise/Gateway/Block/Adminhtml/Config/Edit.php
+++ src/app/code/community/Omise/Gateway/Block/Adminhtml/Config/Edit.php
@@ -1,11 +1,18 @@
 <?php
 class Omise_Gateway_Block_Adminhtml_Config_Edit extends Mage_Adminhtml_Block_Widget_Form_Container
 {
+    /**
+     * Note:
+     * Here is an internal constructor (different from real __construct() one).
+     * (It's recommened by Magento to override this method instead of __construct()).
+     *
+     * @see Magento: Mage/Core/Block/Abstract
+     */
     protected function _construct()
     {
-        $this->_blockGroup  = 'omise_gateway_adminhtml';
-        $this->_controller  = 'config';
-        $this->_mode        = 'edit';
-        $this->_headerText  =  $this->__('Omise Gateway Configuration');
+        $this->_blockGroup = 'omise_gateway_adminhtml';
+        $this->_controller = 'config';
+        $this->_mode       = 'edit';
+        $this->_headerText =  $this->__('Omise Gateway Configuration');
     }
 }
--- src/app/code/community/Omise/Gateway/Model/Omise.php
+++ src/app/code/community/Omise/Gateway/Model/Omise.php
@@ -9,7 +9,11 @@ class Omise_Gateway_Model_Omise extends Mage_Core_Model_Abstract
     /**
      * Load necessary file and setup Omise keys
      *
-     * @return void
+     * Note:
+     * Here is an internal constructor (different from real __construct() one).
+     * (It's recommened by Magento to override this method instead of __construct()).
+     *
+     * @see Magento: lib/Varien/Object.php
      */
     protected function _construct()
     {
