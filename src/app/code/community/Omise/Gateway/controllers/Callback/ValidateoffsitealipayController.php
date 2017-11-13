86fb95fe9da6f31bb5eea79ac0ac475abba4b20f
guzzilar <iam.nuttanon@gmail.com>
2017-11-13 10:44:14 +0700

    Be able to place order with Alipay payment at the checkout page.

---
+++ src/app/code/community/Omise/Gateway/controllers/Callback/ValidateoffsitealipayController.php
@@ -0,0 +1,9 @@
+<?php
+class Omise_Gateway_Callback_ValidateoffsitealipayController extends Omise_Gateway_Controller_Base
+{
+    public function indexAction()
+    {
+        // Callback validation.
+
+    }
+}
