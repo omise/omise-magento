eeff6d635b45b63d4a3c61c4366afd9b1f15c03a
guzzilar <iam.nuttanon@gmail.com>
2017-12-22 17:08:30 +0700

    Retrieve an Event Object from Omise API when Webhook callback is activated.

--- Controller/Callback/Webhook.php
+++ Controller/Callback/Webhook.php
@@ -1,30 +1,38 @@
 <?php
+
 namespace Omise\Payment\Controller\Callback;
 
-use Exception;
-use Omise\Payment\Model\Config\Cc as Config;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Omise\Payment\Model\Omise;
+use Omise\Payment\Model\Api\Event;
+use Omise\Payment\Model\Api\Error;
 
-class Webhook extends \Magento\Framework\App\Action\Action
+class Webhook extends Action
 {
     /**
-     * @var \Magento\Checkout\Model\Session
+     * @var Omise\Payment\Model\Omise
      */
-    protected $session;
+    protected $omise;
 
     /**
-     * @var \Omise\Payment\Model\Config\Cc
+     * @var \Omise\Payment\Model\Api\Event
      */
-    protected $config;
+    protected $event;
 
     public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Magento\Checkout\Model\Session $session,
-        Config  $config
+        Context $context,
+        Omise   $omise,
+        Event   $event
     ) {
+        $this->omise = $omise;
+        $this->event = $event;
+
         parent::__construct($context);
 
-        $this->session = $session;
-        $this->config  = $config;
+        $this->omise->defineUserAgent();
+        $this->omise->defineApiVersion();
+        $this->omise->defineApiKeys();
     }
 
     /**
@@ -32,6 +40,25 @@ class Webhook extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-        
+         if (! $this->getRequest()->isPost()) {
+            // TODO: Only accept for POST verb.
+            return;
+        }
+
+        $payload = json_decode(file_get_contents('php://input'));
+
+        if ($payload->object !== 'event' || ! $payload->id) {
+            // TODO: Handle in case of improper response structure.
+            return;
+        }
+
+        $event = $this->event->find($payload->id);
+
+        if ($event instanceof Error) {
+            // TODO: Handle in case can't retrieve an event object from '$payload->id'.
+            return;
+        }
+
+        var_dump($event); exit;
     }
 }
---
+++ Model/Api/Event.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Omise\Payment\Model\Api;
+
+use Exception;
+use OmiseEvent;
+
+/**
+ * @property string $object
+ * @property string $id
+ * @property bool   $livemode
+ * @property string $location
+ * @property string $key
+ * @property string $created
+ * @property Object $data
+ * @see      https://www.omise.co/events-api
+ */
+class Event extends Object
+{
+    /**
+     * @param  string $id
+     *
+     * @return \Omise\Payment\Model\Api\Error|self
+     */
+    public function find($id)
+    {
+        try {
+            $event         = OmiseEvent::retrieve($id);
+            $event['data'] = $this->transformDataToObject($event['data']);
+            $this->refresh($event);
+        } catch (Exception $e) {
+            return new Error([
+                'code'    => 'not_found',
+                'message' => $e->getMessage()
+            ]);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param  json-object $data
+     *
+     * @return \Omise\Payment\Model\Api\Error|json-object
+     */
+    protected function transformDataToObject($data)
+    {
+        switch ($data['object']) {
+            case 'charge':
+                $data = (new Charge)->find($data['id']);
+                break;
+        }
+
+        return $data;
+    }
+}
