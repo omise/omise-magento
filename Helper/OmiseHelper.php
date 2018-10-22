ecdc62576156a8a29d8d6e79d50dea9d4edb04a5
Jacek <jacek.stanusz@gmail.com>
2018-10-22 12:27:47 +0700

    convert SVG to html - new Omise Helper Functionality

--- Helper/OmiseHelper.php
+++ Helper/OmiseHelper.php
@@ -3,6 +3,8 @@ namespace Omise\Payment\Helper;
 
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Store\Model\ScopeInterface;
+use SimpleXMLElement;
+use DOMDocument;
 
 class OmiseHelper extends AbstractHelper
 {
@@ -38,5 +40,38 @@ class OmiseHelper extends AbstractHelper
         }
 
         return $amount;
+    }
+
+
+    public function convertSVGToHTML($svg)
+    {
+        return 'hahahahahah';
+        $xml = new SimpleXMLElement($svg);
+        if (!$xml)
+        {
+            return 'ERROR PARSING XML';
+        }
+
+        $node = $xml->children()->children();
+        $xhtml = new DOMDocument();
+        foreach ($node as $child)
+        {
+            if ($child->getName() === 'g') {
+                $prevX = 0;
+                $prevWidth = 0;
+                
+                foreach ($child->children() as $rect) {
+                    $attrArr = $rect->attributes();
+                    $divRect = $xhtml->createElement('div');
+                    $width = $attrArr['width'];
+                    $margin =($attrArr['x'] - $prevX - $prevWidth) . 'px';
+                    $divRect->setAttribute('style', "float:left;position:relative;height:50px; width:$width; background-color:#000; margin-left:$margin");
+                    $xhtml->appendChild($divRect);
+                    $prevX = $attrArr['x'];
+                    $prevWidth = $attrArr['width'];
+                }
+            }
+        }
+        return $xhtml->saveXML(null, LIBXML_NOEMPTYTAG);
     }
 }
