1a422f238d4856572100d5fcf8991219b5acaec0
Jacek <jacek.stanusz@gmail.com>
2019-03-21 10:50:55 +0700

    delete card, added basic block files, routing

---
+++ Block/Customer/PaymentMethods.php
@@ -0,0 +1,53 @@
+<?php
+namespace Omise\Payment\Block\Customer;
+
+use Magento\Framework\View\Element\Template;
+use Omise\Payment\Model\Customer;
+
+/**
+ * Class PaymentTokens
+ */
+class PaymentMethods extends Template
+{
+    /**
+     * @var PSR\Log\LoggerInterface
+     */
+    private $log;
+
+    /**
+     * @var Omise\Payment\Model\Customer;
+     */
+    private $customer;
+
+    /**
+     * PaymentMethods constructor.
+     * @param Template\Context $context
+     * @param Omise\Payment\Model\Customer $customer
+     * @param PSR\Log\LoggerInterface $log
+     * @param array $data
+     */
+    public function __construct(
+        Template\Context $context,
+        Customer $customer,
+        \PSR\Log\LoggerInterface $log,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->log = $log;
+        $this->customer = $customer;
+    }
+
+    /**
+     * @return  array
+     */
+    public function getCards()
+    {
+        $this->log->debug('getting cards');
+        if (! $this->customer->getMagentoCustomerId() || ! $this->customer->getId()) {
+            $this->log->debug('no customer id or magento customer id', ['magid'=> $this->customer->getMagentoCustomerId()]);
+            return null;
+        }
+        $this->log->debug('list of cards', ['cards'=>$this->customer->cards(['order' => 'reverse_chronological'])]);
+        return $this->customer->cards(['order' => 'reverse_chronological']);
+    }
+}
---
+++ Controller/Methods/Index.php
@@ -0,0 +1,64 @@
+<?php
+namespace Omise\Payment\Controller\Methods;
+
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Exception\NotFoundException;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var Session
+     */
+    protected $customerSession;
+
+    /**
+     * @var Magento\Framework\View\Result\PageFactory
+     */
+    private $pageFactory;
+
+    /**
+     * @param Context $context
+     * @param Session $customerSession
+     */
+    public function __construct(
+        Context $context,
+        Session $customerSession,
+        \Magento\Framework\View\Result\PageFactory $pageFactory
+    ) {
+        parent::__construct($context);
+        $this->customerSession = $customerSession;
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * Dispatch request
+     *
+     * @param RequestInterface $request
+     * @return ResponseInterface
+     * @throws NotFoundException
+     */
+    public function dispatch(RequestInterface $request)
+    {
+        if (!$this->customerSession->authenticate()) {
+            $this->_actionFlag->set('', 'no-dispatch', true);
+        }
+        return parent::dispatch($request);
+    }
+
+    /**
+     * Dispatch request
+     *
+     * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+     * @throws \Magento\Framework\Exception\NotFoundException
+     */
+    public function execute()
+    {
+        $resultPage = $this->pageFactory->create();
+        $resultPage->getConfig()->getTitle()->set(__('Stored Payment Methods'));
+
+        return $resultPage;
+    }
+}
