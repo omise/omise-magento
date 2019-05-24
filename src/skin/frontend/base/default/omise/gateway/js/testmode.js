048df52b88d3addc99dde57e06387e496e2cd3c7
Jon Randy <jonrandy@gmail.com>
2019-05-24 16:00:01 +0700

    Adds a link to Omise help page about keys

--- src/skin/frontend/base/default/omise/gateway/js/testmode.js
+++ src/skin/frontend/base/default/omise/gateway/js/testmode.js
@@ -1,7 +1,7 @@
 
 window.addEventListener('load', function() {
 	var warning = document.createElement('div');
-	warning.innerHTML = 'Omise Test Mode';
+	warning.innerHTML = 'OMISE TEST MODE - <a target="_blank" href="https://www.omise.co/what-are-test-keys-and-live-keys">[more info here]</a>';
 	warning.className = 'omise-test-warning';
 	document.body.appendChild(warning);
 });
