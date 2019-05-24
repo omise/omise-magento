154930c518ee246234ece8532c370070a6ebc4e8
Jon Randy <jonrandy@gmail.com>
2019-05-24 16:23:42 +0700

    Makes warning text multilingual

--- src/skin/frontend/base/default/omise/gateway/js/testmode.js
+++ src/skin/frontend/base/default/omise/gateway/js/testmode.js
@@ -1,7 +1,9 @@
 
 window.addEventListener('load', function() {
-	var warning = document.createElement('div');
-	warning.innerHTML = 'OMISE TEST MODE - <a target="_blank" href="https://www.omise.co/what-are-test-keys-and-live-keys">[more info here]</a>';
+	var warning = document.createElement('div'); 
+	warning.innerHTML = Translator.translate('OMISE TEST MODE') + ' - \
+		<a target="_blank" href="https://www.omise.co/what-are-test-keys-and-live-keys">\
+		[ ' + Translator.translate('more info') +' ]</a>';
 	warning.className = 'omise-test-warning';
 	document.body.appendChild(warning);
 });
