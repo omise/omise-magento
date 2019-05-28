cc7bb6b7abb439c59dd79b220a0c845a8cc7c7ed
Jacek <jacek.stanusz@gmail.com>
2019-05-28 14:01:52 +0200

    truemoney - added config, di.xml, admin stuff

---
+++ Model/Config/Truemoney.php
@@ -0,0 +1,12 @@
+<?php
+namespace Omise\Payment\Model\Config;
+
+use Omise\Payment\Model\Config\Config;
+
+class Truemoney extends Config
+{
+    /**
+     * @var string
+     */
+    const CODE = 'omise_offsite_truemoney';
+}
