056bc55c5f3e3f3be9b72b2b35eac4d8c714bc69
Jacek <jacek.stanusz@gmail.com>
2019-09-06 11:11:56 +0700

    Installment Config - added config constants for particular installment option

--- Model/Config/Installment.php
+++ Model/Config/Installment.php
@@ -9,4 +9,10 @@ class Installment extends Config
      * @var string
      */
     const CODE = 'omise_offsite_installment';
+
+    const KBANK       = 'omise_offsite_installment_kbank';
+    const KRUNGTHAI   = 'omise_offsite_installment_krungthai';
+    const FIRSTCHOICE = 'omise_offsite_installment_fc';
+    const BBL         = 'omise_offsite_installment_bbl';
+    const KRUNGSRI    = 'omise_offsite_installment_krungsri';
 }
