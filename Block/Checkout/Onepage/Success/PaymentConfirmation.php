1a468edbc377f048f5b46db428e8ddebd0be883b
Jacek <jacek.stanusz@gmail.com>
2019-09-23 14:34:56 +0700

    payment confirmation - basic view

---
+++ Block/Checkout/Onepage/Success/PaymentConfirmation.php
@@ -0,0 +1,44 @@
+<?php
+namespace Omise\Payment\Block\Checkout\Onepage\Success;
+
+class PaymentConfirmation extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * @var \Magento\Checkout\Model\Session
+     */
+    protected $_checkoutSession;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        array $data = []
+    ) {
+        $this->_checkoutSession = $checkoutSession;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Return HTML code with tesco lotus payment infromation
+     *
+     * @return string
+     */
+    protected function _toHtml()
+    {
+        $paymentData = $this->_checkoutSession->getLastRealOrder()->getPayment()->getData();
+        // if (!isset($paymentData['additional_information']['payment_type']) || $paymentData['additional_information']['payment_type'] !== 'bill_payment_tesco_lotus') {
+        //     return;
+        // }
+        $orderCurrency = $this->_checkoutSession->getLastRealOrder()->getOrderCurrency()->getCurrencyCode();
+
+        $this->addData([
+            'confirmation' => 'Hello'
+        ]);
+        
+        return parent::_toHtml();
+    }
+}
