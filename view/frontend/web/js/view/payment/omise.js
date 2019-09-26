554380f08352b5ee7fd9c0f6266124e13d5d273b
Jon Randy <jonrandy@gmail.com>
2019-09-26 14:39:24 +0700

    Removes forgotten commented code

--- view/frontend/web/js/view/payment/omise.js
+++ view/frontend/web/js/view/payment/omise.js
@@ -27,48 +27,6 @@ define(
             });  
         });
 
-        // rendererList.push(
-        //     {
-        //         type: 'omise_cc',
-        //         component: 'Omise_Payment/js/view/payment/method-renderer/omise-cc-method'
-        //     }
-        // );
-
-        // rendererList.push(
-        //     {
-        //         type: 'omise_offsite_internetbanking',
-        //         component: 'Omise_Payment/js/view/payment/method-renderer/omise-offsite-internetbanking-method'
-        //     }
-        // );
-
-        // rendererList.push(
-        //     {
-        //         type: 'omise_offsite_alipay',
-        //         component: 'Omise_Payment/js/view/payment/method-renderer/omise-offsite-alipay-method'
-        //     }
-        // );
-
-        // rendererList.push(
-        //     {
-        //         type: 'omise_offsite_installment',
-        //         component: 'Omise_Payment/js/view/payment/method-renderer/omise-offsite-installment-method'
-        //     }
-        // );
-
-        // rendererList.push(
-        //     {
-        //         type: 'omise_offsite_truemoney',
-        //         component: 'Omise_Payment/js/view/payment/method-renderer/omise-offsite-truemoney-method'
-        //     }
-        // );
-
-        // rendererList.push(
-        //     {
-        //         type: 'omise_offline_tesco',
-        //         component: 'Omise_Payment/js/view/payment/method-renderer/omise-offline-tesco-method'
-        //     }
-        // );
-
         return Component.extend({});
     }
 );
