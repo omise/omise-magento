89f4fd7158459175e65608d78064bd2a8dc15ba9
Jon Randy <jonrandy@gmail.com>
2019-09-26 13:57:49 +0700

    Improves maintainability of method-renderer setup

--- view/frontend/web/js/view/payment/omise.js
+++ view/frontend/web/js/view/payment/omise.js
@@ -9,47 +9,63 @@ define(
     ) {
         'use strict';
 
-        rendererList.push(
-            {
-                type: 'omise_cc',
-                component: 'Omise_Payment/js/view/payment/method-renderer/omise-cc-method'
-            }
-        );
+        const METHOD_RENDERERS = [
+            'cc',
+            'offsite-internetbanking',
+            'offsite-alipay',
+            'offsite-installment',
+            'offsite-truemoney',
+            'offline-tesco'
+        ];
 
-        rendererList.push(
-            {
-                type: 'omise_offsite_internetbanking',
-                component: 'Omise_Payment/js/view/payment/method-renderer/omise-offsite-internetbanking-method'
-            }
-        );
+        METHOD_RENDERERS.forEach(rendererName => {
+            rendererList.push({
+                type: 'omise_' + rendererName.replace(/-/g, '_'),
+                component: 'Omise_Payment/js/view/payments/method-renderer/omise-' + rendererName + '-method'
+            });  
+        });
 
-        rendererList.push(
-            {
-                type: 'omise_offsite_alipay',
-                component: 'Omise_Payment/js/view/payment/method-renderer/omise-offsite-alipay-method'
-            }
-        );
+        // rendererList.push(
+        //     {
+        //         type: 'omise_cc',
+        //         component: 'Omise_Payment/js/view/payment/method-renderer/omise-cc-method'
+        //     }
+        // );
 
-        rendererList.push(
-            {
-                type: 'omise_offsite_installment',
-                component: 'Omise_Payment/js/view/payment/method-renderer/omise-offsite-installment-method'
-            }
-        );
+        // rendererList.push(
+        //     {
+        //         type: 'omise_offsite_internetbanking',
+        //         component: 'Omise_Payment/js/view/payment/method-renderer/omise-offsite-internetbanking-method'
+        //     }
+        // );
 
-        rendererList.push(
-            {
-                type: 'omise_offsite_truemoney',
-                component: 'Omise_Payment/js/view/payment/method-renderer/omise-offsite-truemoney-method'
-            }
-        );
+        // rendererList.push(
+        //     {
+        //         type: 'omise_offsite_alipay',
+        //         component: 'Omise_Payment/js/view/payment/method-renderer/omise-offsite-alipay-method'
+        //     }
+        // );
 
-        rendererList.push(
-            {
-                type: 'omise_offline_tesco',
-                component: 'Omise_Payment/js/view/payment/method-renderer/omise-offline-tesco-method'
-            }
-        );
+        // rendererList.push(
+        //     {
+        //         type: 'omise_offsite_installment',
+        //         component: 'Omise_Payment/js/view/payment/method-renderer/omise-offsite-installment-method'
+        //     }
+        // );
+
+        // rendererList.push(
+        //     {
+        //         type: 'omise_offsite_truemoney',
+        //         component: 'Omise_Payment/js/view/payment/method-renderer/omise-offsite-truemoney-method'
+        //     }
+        // );
+
+        // rendererList.push(
+        //     {
+        //         type: 'omise_offline_tesco',
+        //         component: 'Omise_Payment/js/view/payment/method-renderer/omise-offline-tesco-method'
+        //     }
+        // );
 
         return Component.extend({});
     }
