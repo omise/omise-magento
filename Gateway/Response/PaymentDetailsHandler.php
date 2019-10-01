788cff56a8addc688570e9f4f866ab5b4520e8d0
Jacek <jacek.stanusz@gmail.com>
2019-10-01 17:11:38 +0700

    Payment details handler, revert file

--- Gateway/Response/PaymentDetailsHandler.php
+++ Gateway/Response/PaymentDetailsHandler.php
@@ -1,29 +1,21 @@
 <?php
 namespace Omise\Payment\Gateway\Response;
-
 use Magento\Payment\Gateway\Helper\SubjectReader;
 use Magento\Payment\Gateway\Response\HandlerInterface;
-
 class PaymentDetailsHandler implements HandlerInterface
 {
-    private $log;
-    public function __construct(\PSR\LoggerInterface\Log $log) {
-        $this->log = $log;
-    }
     /**
      * @param string $url URL to Tesco Barcode generated in Omise Backend
      * @return string Barcode in SVG format
      */
-    private function downloadTescoBarcode($url)
+    private function downloadTescoBarcode($url) 
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
         return curl_exec($ch);
     }
-
     /**
      * @inheritdoc
      */
@@ -31,20 +23,13 @@ class PaymentDetailsHandler implements HandlerInterface
     {
         $payment = SubjectReader::readPayment($handlingSubject);
         $payment = $payment->getPayment();
-
         $payment->setAdditionalInformation('charge_id', $response['charge']->id);
         $payment->setAdditionalInformation('charge_authorize_uri', $response['charge']->authorize_uri);
         $payment->setAdditionalInformation('payment_type', $response['charge']->source['type']);
-        $this->log->debug ('payment details - subject handler', ['response'=> $response['charge']]);
-
-        switch ($response['charge']->source['type']) {
-            case 'bill_payment_tesco_lotus':
-                $barcode = $this->downloadTescoBarcode($response['charge']->source['references']['barcode']);
-                $payment->setAdditionalInformation('barcode', $barcode);
-                break;
-            case 'econtext':
-                $payment->setAdditionalInformation('payment_link', $response['charge']['authorize_uri']);
-                break;
+        
+        if ($response['charge']->source['type'] === 'bill_payment_tesco_lotus') {
+            $barcode = $this->downloadTescoBarcode($response['charge']->source['references']['barcode']);
+            $payment->setAdditionalInformation('barcode', $barcode);
         }
     }
 }
