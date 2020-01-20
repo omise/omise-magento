90978bcb0ad3b839c52b67c1e5cd7afd170aa5e8
Mayur Kathale <mayur.kathale@gmail.com>
2020-01-20 13:36:32 +0700

    True Money (PR change) - Populating telephone number from billing address

--- src/app/code/community/Omise/Gateway/Block/Form/Offsitetruemoney.php
+++ src/app/code/community/Omise/Gateway/Block/Form/Offsitetruemoney.php
@@ -1,7 +1,6 @@
 <?php
 class Omise_Gateway_Block_Form_Offsitetruemoney extends Mage_Payment_Block_Form
 {
-
     /**
      * Preparing global layout
      * You can redefine this method in child classes for changing layout
@@ -27,8 +26,18 @@ class Omise_Gateway_Block_Form_Offsitetruemoney extends Mage_Payment_Block_Form
             'payment_form_block_to_html_before',
             array('block' => $this)
         );
-
         return parent::_toHtml();
     }
-    
+
+    /**
+     * Returns billing phone number from current quote
+     * @return string
+     */
+    public function getOmiseTruePhoneNumber() {
+        $phoneNumber = Mage::getSingleton('checkout/session')
+            ->getQuote()
+            ->getBillingAddress()
+            ->getTelephone();
+        return (isset($phoneNumber)) ? $phoneNumber : '';
+     }
 }
