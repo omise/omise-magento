46b992c4d5c794d9cc8d34c9c92ff49b165b64cc
Mayur Kathale <mayur.kathale@gmail.com>
2020-04-22 13:00:00 +0700

    Adding url path check specific to webhook

--- Plugin/CsrfValidatorSkip.php
+++ Plugin/CsrfValidatorSkip.php
@@ -3,6 +3,10 @@ namespace Omise\Payment\Plugin;
 
 class CsrfValidatorSkip
 {
+    protected $urlInterface;
+    public function __construct(\Magento\Framework\UrlInterface $urlInterface) {
+        $this->urlInterface = $urlInterface;
+    }
     /**
      * @param \Magento\Framework\App\Request\CsrfValidator $subject
      * @param \Closure $proceed
@@ -15,7 +19,7 @@ class CsrfValidatorSkip
         $request,
         $action
     ) {
-        if ($request->getModuleName() == 'omise') {
+        if ($request->getModuleName() == 'omise' && strpos($this->urlInterface->getCurrentUrl(), 'omise/callback/webhook')) {
             return;
         }
         $proceed($request, $action);
