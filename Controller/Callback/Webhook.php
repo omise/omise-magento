d8ee7e55a43149194c791f6982f18a8c2fd26b2a
Mayur Kathale <mayur.kathale@gmail.com>
2020-04-22 12:18:46 +0700

    Changing to another solution which supports older versions of magento to fix webhooks to execute POST requests

--- Controller/Callback/Webhook.php
+++ Controller/Callback/Webhook.php
@@ -2,14 +2,11 @@
 
 namespace Omise\Payment\Controller\Callback;
 
-use Magento\Framework\App\CsrfAwareActionInterface;
-use Magento\Framework\App\RequestInterface;
-use Magento\Framework\App\Request\InvalidRequestException;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Omise\Payment\Model\Event;
 
-class Webhook extends Action implements CsrfAwareActionInterface
+class Webhook extends Action
 {
     /**
      * @var \Omise\Payment\Model\Event
@@ -25,24 +22,6 @@ class Webhook extends Action implements CsrfAwareActionInterface
         $this->event = $event;
 
         parent::__construct($context);
-    }
-
-    /**
-     * @param RequestInterface $request
-     * @return InvalidRequestException|null
-     */
-    public function createCsrfValidationException(RequestInterface $request): ? InvalidRequestException
-    {
-        return null;
-    }
-    
-    /**
-     * @param RequestInterface $request
-     * @return boolean|null
-     */
-    public function validateForCsrf(RequestInterface $request): ?bool
-    {
-        return true;
     }
 
     /**
---
+++ Plugin/CsrfValidatorSkip.php
@@ -0,0 +1,23 @@
+<?php
+namespace Omise\Payment\Plugin;
+
+class CsrfValidatorSkip
+{
+    /**
+     * @param \Magento\Framework\App\Request\CsrfValidator $subject
+     * @param \Closure $proceed
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @param \Magento\Framework\App\ActionInterface $action
+     */
+    public function aroundValidate(
+        $subject,
+        \Closure $proceed,
+        $request,
+        $action
+    ) {
+        if ($request->getModuleName() == 'omise') {
+            return;
+        }
+        $proceed($request, $action);
+    }
+}
