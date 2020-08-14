decf3483152949409604c4016164208110a81893
Mayur Kathale <mayur.kathale@gmail.com>
2020-08-14 15:18:37 +0700

    addressing PR comment - moving email code out of observer

---
+++ Model/Data/Email.php
@@ -0,0 +1,127 @@
+<?php
+namespace Omise\Payment\Model\Data;
+
+class Email
+{
+    /**
+    * @var \Magento\Framework\App\Config\ScopeConfigInterface
+    */
+    protected $_scopeConfig;
+
+    /**
+     * @var \Omise\Payment\Model\Api\Charge
+     */
+    protected $_charge;
+
+    /**
+     * @var \Magento\Framework\View\Asset\Repository
+     */
+    protected $_assetRepo;
+
+    /**
+    * @var  \Magento\Framework\Mail\Template\TransportBuilder
+    */
+    private $_transportBuilder;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $_storeManager;
+
+    /**
+     * @var string
+     */
+    private $storeName;
+
+    /**
+     * @var string
+     */
+    private $emailTemplate;
+
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Omise\Payment\Model\Api\Charge  $charge,
+        \Magento\Framework\View\Asset\Repository $assetRepo,
+        \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
+    ) {
+        $this->_scopeConfig = $scopeConfig;
+        $this->_charge      = $charge;
+        $this->_assetRepo = $assetRepo;
+        $this->_transportBuilder = $transportBuilder;
+        $this->_storeManager = $storeManager;
+    }
+
+    /**
+     * @param \Magento\Sales\Model\Order $order
+     * @return void
+     */
+    public function sendEmail($order) {
+        $this->storeName = $this->_scopeConfig->getValue('trans_email/ident_sales/name', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        $emailData       = $this->getEmailData($order);
+        $transport       = $this->_transportBuilder
+            ->setTemplateIdentifier($this->emailTemplate)
+            ->setTemplateOptions([
+                'area' => \Magento\Framework\App\Area::AREA_FRONTEND,
+                'store' => \Magento\Store\Model\Store::DEFAULT_STORE_ID,
+            ])
+            ->setTemplateVars(['data' => $emailData])
+            ->setFrom([
+                'name'  => $this->storeName,
+                'email' => $this->_scopeConfig->getValue('trans_email/ident_sales/email', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);,
+            ])
+            ->addTo($order->getCustomerEmail())
+            ->getTransport();
+        $transport->sendMessage();
+    }
+    /**
+     * @param \Magento\Sales\Model\Order $order
+     * @return \Magento\Framework\DataObject
+     */
+    private function getEmailData($order) {
+        $emailData   = new \Magento\Framework\DataObject();
+        $payment     = $order->getPayment();
+        $paymentType = $payment->getAdditionalInformation('payment_type');
+        $charge      = $this->_charge->find($payment->getAdditionalInformation('charge_id'));
+        switch($paymentType) {
+            case 'bill_payment_tesco_lotus':
+                // make sure timezone is Thailand.
+                date_default_timezone_set("Asia/Bangkok");
+                $emailData->addData(['barcode' => $this->_helper->convertTescoSVGCodeToHTML($payment->getAdditionalInformation('barcode'))]);
+                $emailData->addData(['validUntil' => date("d-m-Y H:i:s" , strtotime($charge->expires_at))]);
+                $this->emailTemplate = 'send_email_tesco_template';
+                break;
+            case 'paynow':
+                // make sure timezone is Singapore.
+                date_default_timezone_set("Asia/Singapore");
+                $emailData->addData(['barcode' => "<img src= '".$charge->source['scannable_code']['image']['download_uri']."'/>"]);
+                $emailData->addData(['banksUrl' => $this->_assetRepo->getUrl('Omise_Payment::images/paynow_supportedbanks.png')]);
+                $emailData->addData(['validUntil' => $this->getPaynowChargeExpiryTime()]);
+                $this->emailTemplate = 'send_email_paynow_template';
+                break;
+            case 'promptpay':
+                // make sure timezone is Thailand.
+                date_default_timezone_set("Asia/Bangkok");
+                $emailData->addData(['barcode' => $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_WEB)."omise/payment/displayqr/orderId/".$order->getIncrementId()]);
+                $emailData->addData(['validUntil' => date("d-m-Y H:i:s" , strtotime('+1 day'))]);
+                $this->emailTemplate = 'send_email_promptpay_template';
+                break;
+            default:
+                return $this;
+        }
+        $paymentData = $payment->getData();
+        $emailData->addData(['amount' => number_format($paymentData['amount_ordered'], 2) . ' ' . $order->getOrderCurrency()->getCurrencyCode()]);
+        $emailData->addData(['orderId' => $order->getIncrementId()]);
+        $emailData->addData(['storename' => $this->storeName]);
+        return $emailData;
+    }
+
+    /**
+     * calculates expiry time for paynow charge
+     * @return string
+     */
+    private function getPaynowChargeExpiryTime() {
+        $timestamp = time() > strtotime('tomorrow - 1seconds') ? strtotime('tomorrow + 1day - 1second') : strtotime('tomorrow - 1seconds');
+        return date("d-m-Y H:i:s" , $timestamp);
+    }
+}
--- Observer/OfflinePaymentObserver.php
+++ Observer/OfflinePaymentObserver.php
@@ -6,58 +6,24 @@ use Magento\Framework\Event\Observer;
 
 class OfflinePaymentObserver implements ObserverInterface
 {
-    /**
-    * @var \Magento\Framework\App\Config\ScopeConfigInterface
-    */
-    private $_scopeConfig;    
+    
     
     /**
-    * @var  \Magento\Framework\Mail\Template\TransportBuilder
-    */
-    private $_transportBuilder;
-    
-    /**
-    * @var  \Omise\Payment\Helper
+    * @var  \Omise\Payment\Helper\OmiseHelper
     */
     private $_helper;
 
-    /**
-     * @var \Omise\Payment\Model\Api\Charge
-     */
-    protected $charge;
-
-    /**
-     * @var \Magento\Framework\View\Asset\Repository
-     */
-    protected $_assetRepo;
-
-    /**
-     * @var \Magento\Store\Model\StoreManagerInterface
-     */
-    protected $_storeManager;
+    private $_email;
 
     /**
      * @param \Omise\Payment\Helper\OmiseHelper $helper
-     * @param \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder
-     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
-     * @param \Magento\Framework\View\Asset\Repository $assetRepo
-     * @param \Omise\Payment\Model\Api\Charge $charge
-     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
      */
     public function __construct(
         \Omise\Payment\Helper\OmiseHelper $helper,
-        \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Magento\Framework\View\Asset\Repository $assetRepo,
-        \Omise\Payment\Model\Api\Charge  $charge,
-        \Magento\Store\Model\StoreManagerInterface $storeManager
+        \Omise\Payment\Model\Data\Email $email
     ) {
-        $this->_scopeConfig      = $scopeConfig;
         $this->_helper           = $helper;
-        $this->_transportBuilder = $transportBuilder;
-        $this->_assetRepo = $assetRepo;
-        $this->charge  = $charge;
-        $this->_storeManager = $storeManager;
+        $this->_email           = $email;
     }
 
     /**
@@ -67,70 +33,10 @@ class OfflinePaymentObserver implements ObserverInterface
     public function execute(Observer $observer)
     {
         $order   = $observer->getEvent()->getOrder();
-        $payment = $order->getPayment();
-        $emailData     = new \Magento\Framework\DataObject();
-        $charge_id = $payment->getAdditionalInformation('charge_id');
-        $charge = $this->charge->find($charge_id);
-        switch($payment->getAdditionalInformation('payment_type')) {
-            case 'bill_payment_tesco_lotus':
-                // make sure timezone is Thailand.
-                date_default_timezone_set("Asia/Bangkok");
-                $codeTemplate  = $this->_helper->convertTescoSVGCodeToHTML($payment->getAdditionalInformation('barcode'));
-                $emailTemplate = 'send_email_tesco_template';
-                $validUntil    = date("d-m-Y H:i:s" , strtotime($charge->expires_at));
-                break;
-            case 'paynow':
-                // make sure timezone is Singapore.
-                date_default_timezone_set("Asia/Singapore");
-                $codeTemplate   = "<img src= '".$charge->source['scannable_code']['image']['download_uri']."'/>";
-                $emailTemplate = 'send_email_paynow_template';
-                $emailData->setData(['banksUrl' => $this->_assetRepo->getUrl('Omise_Payment::images/paynow_supportedbanks.png')]);
-                $validUntil = $this->getPaynowChargeExpiryTime();
-                break;
-            case 'promptpay':
-                // make sure timezone is Thailand.
-                date_default_timezone_set("Asia/Bangkok");
-                $codeTemplate   = $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_WEB)."omise/payment/displayqr/orderId/".$order->getIncrementId();
-                $emailTemplate = 'send_email_promptpay_template';
-                $validUntil    = date("d-m-Y H:i:s" , strtotime('+1 day'));
-                break;
-            default:
-                return $this;
+        $paymentType = $order->getPayment()->getAdditionalInformation('payment_type');
+        if($this->_helper->isPayableByImageCode($paymentType)) {
+            $this->_email->sendEmail($order);
         }
-        $paymentData   = $payment->getData();
-        $amount        = number_format($paymentData['amount_ordered'], 2) . ' ' . $order->getOrderCurrency()->getCurrencyCode();
-        $storeName     = $this->_scopeConfig->getValue('trans_email/ident_sales/name', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-        $storeEmail    = $this->_scopeConfig->getValue('trans_email/ident_sales/email', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-        $customerEmail = $order->getCustomerEmail();
-        $orderId       = $order->getIncrementId();
-        $emailData->setData(['barcode' => $codeTemplate, 'amount' => $amount, 'storename' => $storeName, 'orderId' => $orderId, 'validUntil' => $validUntil]);
-
-        // build and send email
-        $transport = $this->_transportBuilder
-            ->setTemplateIdentifier($emailTemplate)
-            ->setTemplateOptions([
-                'area' => \Magento\Framework\App\Area::AREA_FRONTEND,
-                'store' => \Magento\Store\Model\Store::DEFAULT_STORE_ID,
-            ])
-            ->setTemplateVars(['data' => $emailData])
-            ->setFrom([
-                'name'  => $storeName,
-                'email' => $storeEmail,
-            ])
-            ->addTo($customerEmail)
-            ->getTransport();
-        
-        $transport->sendMessage();
-
         return $this;
-    }
-
-    /**
-     * calculates expiry time for paynow charge
-     * @return string
-     */
-    private function getPaynowChargeExpiryTime() {
-        $timestamp = time() > strtotime('tomorrow - 1seconds') ? strtotime('tomorrow + 1day - 1second') : strtotime('tomorrow - 1seconds');
-        return date("d-m-Y H:i:s" , $timestamp);
     }
 }
