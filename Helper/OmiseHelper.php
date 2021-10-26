897437095c8622c11caccfbd7a78d37cb75adfb3
Kan Srimanchantha <34004612+kan98@users.noreply.github.com>
2021-10-26 09:41:07 +0700

    Update Helper/OmiseHelper.php
    Co-authored-by: Pariwat Hongladarom <61586978+hpariwat@users.noreply.github.com>

--- Helper/OmiseHelper.php
+++ Helper/OmiseHelper.php
@@ -157,7 +157,7 @@ class OmiseHelper extends AbstractHelper
     }
 
     /**
-     * This method checks and return TRUE if $paymentMethod is an offsite paymetn
+     * This method checks and return TRUE if $paymentMethod is an offsite payment
      * otherwise returns false.
      * @param string $paymentMethod
      * @return boolean
