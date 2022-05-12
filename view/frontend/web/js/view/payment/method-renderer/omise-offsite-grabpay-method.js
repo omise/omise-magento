05a96bf25ceca5c79d9e9d3cfc2e517ef56ab3b4
Kan <kan@omise.co>
2022-05-12 15:12:31 +0700

    fix supported currencies

--- view/frontend/web/js/view/payment/method-renderer/omise-offsite-grabpay-method.js
+++ view/frontend/web/js/view/payment/method-renderer/omise-offsite-grabpay-method.js
@@ -21,7 +21,7 @@ define(
             isPlaceOrderActionAllowed: ko.observable(quote.billingAddress() != null),
 
             code: 'omise_offsite_grabpay',
-            restrictedToCurrencies: ['sgd, myr']
+            restrictedToCurrencies: ['sgd', 'myr']
         });
     }
 );
