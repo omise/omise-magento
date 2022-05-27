474129c44e5d9ac64a3409866839143f9084f6b6
Ken <pariwat@omise.co>
2022-05-27 09:54:25 +0700

    Support Google Pay DI

---
+++ Model/Config/CcGooglePay.php
@@ -0,0 +1,12 @@
+<?php
+namespace Omise\Payment\Model\Config;
+
+use Omise\Payment\Model\Config\Config;
+
+class CcGooglePay extends Config
+{
+    /**
+     * @var string
+     */
+    const CODE = 'omise_cc_googlepay';
+}
