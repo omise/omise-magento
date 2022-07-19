33576558e3d4d071dafe05722a62152cb58e7de3
Kanisorn siripatkerdpong <kanisorn@omise.co>
2022-07-19 13:45:14 +0700

    fix name Maybank QR

--- Helper/OmiseHelper.php
+++ Helper/OmiseHelper.php
@@ -190,7 +190,7 @@ class OmiseHelper extends AbstractHelper
         Boost::CODE => "Boost Payment", 
         DuitnowOBW::CODE => "Duitnow Online Banking/Wallets Payment",
         DuitnowQR::CODE => "DuitNow QR Payment",
-        MaybankQR::CODE => "Maybank QRPay Payment",
+        MaybankQR::CODE => "Maybank QRPAY Payment",
         Shopeepay::CODE => "ShopeePay Payment",
 
 
