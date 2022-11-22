7a07da0db4376b88f4d6a4b7202f7cdf2d57b7f2
Zin Kyaw Kyaw <zin@opn.ooo>
2022-11-22 10:39:34 +0700

    [ENGA3-540] : fixed invalid store id

--- Observer/SaleOrderPaymentRefund.php
+++ Observer/SaleOrderPaymentRefund.php
@@ -2,12 +2,13 @@
 
 namespace Omise\Payment\Observer;
 
+use OmiseCharge;
 use Exception;
 use Omise\Payment\Model\Config\Config;
 use Omise\Payment\Helper\OmiseHelper;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Event\Observer;
-use OmiseCharge;
+use Magento\Framework\Exception\LocalizedException;
 
 class SaleOrderPaymentRefund implements ObserverInterface
 {
@@ -20,8 +21,10 @@ class SaleOrderPaymentRefund implements ObserverInterface
      * @param Config $config
      * @param OmiseHelper $helper
      */
-    public function __construct(Config $config, OmiseHelper $helper)
-    {
+    public function __construct(
+        Config $config,
+        OmiseHelper $helper
+    ) {
         $this->config = $config;
         $this->helper = $helper;
     }
@@ -33,17 +36,16 @@ class SaleOrderPaymentRefund implements ObserverInterface
     public function execute(Observer $observer)
     {
         try {
-            $order = $observer->getEvent();
-            if (!$order) {
-                return $this;
-            }
-            $payment = $order->getPayment();
+            $event = $observer->getEvent();
+            $payment = $event->getPayment();
+            $order = $payment->getOrder();
             if (!$payment) {
-                return $this;
+                return new LocalizedException(__('Payment not found'));
             }
             $method = $payment->getMethod();
             // if method contain omise
             if (strpos($method, 'omise') !== false) {
+                $this->config->setStoreId($order['store_id']);
                 $chargeId = $payment->getAdditionalInformation('charge_id');
                 if (!$chargeId) {
                     return $this;
@@ -53,11 +55,23 @@ class SaleOrderPaymentRefund implements ObserverInterface
                     $this->config->getPublicKey(),
                     $this->config->getSecretKey()
                 );
+                if (!$charge['refundable']) {
+                    $methodName = str_replace('_', ' ', $method);
+                    throw new LocalizedException(__('Payment by %1 cannot be refunded.', $methodName));
+                }
                 $amountToRefund = $this->helper->omiseAmountFormat(
                     $charge['currency'],
                     $payment['base_amount_refunded']
                 );
                 $charge->refund(['amount' => $amountToRefund]);
+                $order->addStatusHistoryComment(
+                    __(
+                        'Omise: Payment refunded.<br/>An amount %1 %2 has been refunded.',
+                        number_format($payment['base_amount_refunded'], 2, '.', ''),
+                        $charge['currency']
+                    )
+                );
+                $order->save();
             }
             return $this;
         } catch (Exception $e) {
