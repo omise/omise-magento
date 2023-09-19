23acce0a4e7a76310cbda480dd9f24b33852fad6
(AJ) Zin Kyaw Kyaw <108650842+ajzkk@users.noreply.github.com>
2023-09-19 15:51:10 +0630

    Add OCBC Digital Payment (#443)
    * add ocbc digital payment
    
    * fix typo
    
    * add test for ocbc digital
    
    * update coverage info
    
    * resize ocbc pao image

---
+++ Test/Unit/OcbcDigitalAPMBuilderTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Omise\Payment\Test\Unit;
+
+use Magento\Payment\Gateway\Data\OrderAdapterInterface;
+use Magento\Payment\Gateway\Data\PaymentDataObject;
+use Omise\Payment\Gateway\Request\APMBuilder;
+use Omise\Payment\Helper\OmiseHelper;
+use Omise\Payment\Helper\OmiseMoney;
+use Omise\Payment\Helper\ReturnUrlHelper;
+use Omise\Payment\Model\Capabilities;
+use Omise\Payment\Model\Config\Config;
+use Omise\Payment\Model\Config\OcbcDigital;
+use PHPUnit\Framework\TestCase;
+use Omise\Payment\Test\Mock\InfoMock;
+
+class OcbcDigitalAPMBuilderTest extends TestCase
+{
+    private $builder;
+    private $helper;
+    private $returnUrlHelper;
+    private $config;
+    private $capabilities;
+    private $orderMock;
+    private $infoMock;
+
+    protected function setUp(): void
+    {
+        $this->helper = $this->getMockBuilder(OmiseHelper::class)->disableOriginalConstructor()->getMock();
+        $this->returnUrlHelper = $this->getMockBuilder(ReturnUrlHelper::class)->disableOriginalConstructor()->getMock();
+        $this->config = $this->getMockBuilder(Config::class)->disableOriginalConstructor()->getMock();
+        $this->capabilities = $this->getMockBuilder(Capabilities::class)->disableOriginalConstructor()->getMock();
+        $this->orderMock = $this->getMockBuilder(OrderAdapterInterface::class)->getMock();
+        $this->infoMock = $this->getMockBuilder(InfoMock::class)->getMock();
+    }
+
+    /**
+     * @covers Omise\Payment\Gateway\Request\APMBuilder
+     * @covers Omise\Payment\Model\Config\OcbcDigital
+     */
+    public function testApmBuilder()
+    {
+        $this->infoMock->method('getMethod')->willReturn(OcbcDigital::CODE);
+        $this->returnUrlHelper->method('create')->willReturn([
+            'url' => 'https://omise.co/complete',
+            'token' => '1234'
+        ]);
+
+        $this->builder = new APMBuilder(
+            $this->helper,
+            $this->returnUrlHelper,
+            $this->config,
+            $this->capabilities,
+            new OmiseMoney(),
+        );
+
+        $result = $this->builder->build(['payment' => new PaymentDataObject(
+            $this->orderMock,
+            $this->infoMock
+        )]);
+
+        $this->assertEquals('mobile_banking_ocbc', $result['source']['type']);
+        $this->assertEquals('https://omise.co/complete', $result['return_uri']);
+    }
+
+    /**
+     * @covers Omise\Payment\Model\Config\OcbcDigital
+     */
+    public function testConstants()
+    {
+        $this->assertEquals('omise_offsite_ocbc_digital', OcbcDigital::CODE);
+        $this->assertEquals('mobile_banking_ocbc', OcbcDigital::ID);
+    }
+}
