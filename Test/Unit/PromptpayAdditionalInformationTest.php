167cfa5f9b5d6f0ae1f8f1d33ec45f96ee1a5d80
Zin Kyaw Kyaw <108650842+ajzkk@users.noreply.github.com>
2023-09-28 22:52:28 +0630

    add test for toHtml

--- Test/Unit/PromptpayAdditionalInformationTest.php
+++ Test/Unit/PromptpayAdditionalInformationTest.php
@@ -8,6 +8,9 @@ use Magento\Checkout\Model\Session;
 use Omise\Payment\Test\Mock\PaymentMock;
 use Magento\Framework\View\Element\Template\Context;
 use Omise\Payment\Block\Checkout\Onepage\Success\PromptpayAdditionalInformation;
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Backend\Block\Widget\Grid\Column\Renderer\Currency;
 
 class PromptpayAdditionalInformationTest extends TestCase
 {
@@ -15,6 +18,9 @@ class PromptpayAdditionalInformationTest extends TestCase
     private $checkoutSessionMock;
     private $orderMock;
     private $paymentMock;
+    private $eventManagerMock;
+    private $scopeConfigMock;
+    private $currencyMock;
 
     protected function setUp(): void
     {
@@ -22,24 +28,34 @@ class PromptpayAdditionalInformationTest extends TestCase
         $this->checkoutSessionMock = $this->getMockBuilder(Session::class)->disableOriginalConstructor()->getMock();
         $this->orderMock = $this->getMockBuilder(Order::class)->disableOriginalConstructor()->getMock();
         $this->paymentMock = $this->getMockBuilder(PaymentMock::class)->disableOriginalConstructor()->getMock();
+        $this->eventManagerMock = $this->getMockBuilder(ManagerInterface::class)->disableOriginalConstructor()->getMock();
+        $this->scopeConfigMock = $this->getMockBuilder(ScopeConfigInterface::class)->disableOriginalConstructor()->getMock();
+        $this->currencyMock = $this->getMockBuilder(Currency::class)->disableOriginalConstructor()->getMock();
     }
 
     /**
-     * test expiry date
      * @covers Omise\Payment\Block\Checkout\Onepage\Success\PromptpayAdditionalInformation
      * @covers Omise\Payment\Block\Checkout\Onepage\Success\AdditionalInformation
      */
     public function testChargeExpiryDate()
     {
         $this->paymentMock->method('getData')->willReturn([
+            "amount_ordered" => 1000,
             "additional_information" => [
-                "charge_expires_at" => "2023-09-29T06:49:35Z"
+                "charge_expires_at" => "2023-09-29T06:49:35Z",
+                "payment_type" => "promptpay"
             ]
         ]);
+        $this->contextMock->method('getEventManager')->willReturn($this->eventManagerMock);
+        $this->contextMock->method('getScopeConfig')->willReturn($this->scopeConfigMock);
         $this->orderMock->method('getPayment')->willReturn($this->paymentMock);
+        $this->orderMock->method('getOrderCurrency')->willReturn($this->currencyMock);
         $this->checkoutSessionMock->method('getLastRealOrder')->willReturn($this->orderMock);
         $model = new PromptpayAdditionalInformation($this->contextMock, $this->checkoutSessionMock, []);
 
         $this->assertEquals("Sep 29, 2023 01:49 PM", $model->getChargeExpiryDate());
+
+        $html = $model->toHtml();
+        $this->assertNotNull($html);
     }
 }
