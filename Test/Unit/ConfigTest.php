3429efb2836576efde3b7fdf7ad186258c10ce29
Zin Kyaw Kyaw <108650842+ajzkk@users.noreply.github.com>
2023-10-10 15:04:35 +0630

    add more unit test cases for config

--- Test/Unit/ConfigTest.php
+++ Test/Unit/ConfigTest.php
@@ -26,9 +26,33 @@ class ConfigTest extends TestCase
      * @dataProvider isDynamicWebhooksEnabledProvider
      * @covers Omise\Payment\Model\Config\Config
      */
-    public function testIsDynamicWebhooksEnabled($webhookEnabled, $expected)
+    public function testIsDynamicWebhooksEnabled($webhookEnabled, $dynamicWebhooksEnabled, $expected)
     {
-        $this->scopeConfigMock->shouldReceive('getValue')->andReturn($webhookEnabled);
+        $this->scopeConfigMock->shouldReceive('getValue')
+            ->with('general/locale/code', m::any(), m::any())
+            ->andReturn('en');
+
+        $this->scopeConfigMock->shouldReceive('getValue')
+            ->with('payment/omise/sandbox_status', m::any(), m::any())
+            ->andReturn(1);
+
+        $this->scopeConfigMock->shouldReceive('getValue')
+            ->with('payment/omise/test_public_key', m::any(), m::any())
+            ->andReturn('pkey_test_xx');
+
+        $this->scopeConfigMock->shouldReceive('getValue')
+            ->with('payment/omise/test_secret_key', m::any(), m::any())
+            ->andReturn('pkey_test_xx');
+
+
+        $this->scopeConfigMock->shouldReceive('getValue')
+            ->with('payment/omise/dynamic_webhooks', m::any(), m::any())
+            ->andReturn($dynamicWebhooksEnabled);
+
+        $this->scopeConfigMock->shouldReceive('getValue')
+            ->with('payment/omise/webhook_status', m::any(), m::any())
+            ->andReturn($webhookEnabled);
+
         $this->storeMock->shouldReceive('getId')->andReturn(1);
         $this->storeManagerMock->shouldReceive('getStore')->andReturn($this->storeMock);
 
@@ -45,10 +69,22 @@ class ConfigTest extends TestCase
         return [
             [
                 'webhookEnabled' => 1,
+                'dynamicWebhooksEnabled' => 1,
+                'expected' => true,
+            ],
+            [
+                'webhookEnabled' => 1,
+                'dynamicWebhooksEnabled' => 1,
                 'expected' => true,
             ],
             [
                 'webhookEnabled' => 0,
+                'dynamicWebhooksEnabled' => 1,
+                'expected' => false,
+            ],
+            [
+                'webhookEnabled' => 0,
+                'dynamicWebhooksEnabled' => 0,
                 'expected' => false,
             ],
         ];
