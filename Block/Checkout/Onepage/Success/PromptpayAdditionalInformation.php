7aacb203de1ee19a7067d3873a3ad7fcc6c66a01
(AJ) Zin Kyaw Kyaw <108650842+ajzkk@users.noreply.github.com>
2023-10-20 15:09:17 +0630

    Add QR expires count down for promptpay (#453)
    * add count down for promptpay QR expires
    
    * fix EOF and m2 code standard
    
    * remove false in processFailedCharge
    
    * update ul to ol on listing description
    
    * show payment expired info
    
    * show payment expired with default alert box

--- Block/Checkout/Onepage/Success/PromptpayAdditionalInformation.php
+++ Block/Checkout/Onepage/Success/PromptpayAdditionalInformation.php
@@ -14,20 +14,8 @@ class PromptpayAdditionalInformation extends AdditionalInformation
         }
         $data['order_amount'] = $this->getOrderAmount();
         $data['image_code'] = $this->getPaymentAdditionalInformation('image_code');
-        $data['charge_expires_at'] = $this->getChargeExpiryDate();
+        $data['charge_expires_at'] = $this->getPaymentAdditionalInformation('charge_expires_at');
         $this->addData($data);
         return parent::_toHtml();
-    }
-
-    /**
-     * get expiry date
-     * @return string
-     */
-    public function getChargeExpiryDate()
-    {
-        // Making sure that timezone is Thailand
-        date_default_timezone_set("Asia/Bangkok");
-        $timestamp = strtotime($this->getPaymentAdditionalInformation('charge_expires_at'));
-        return date("M d, Y h:i A", $timestamp);
     }
 }
