9d6abebb836fc4e081e7eecfc651a7d21c9efa1d
Pratik Bharodiya <bharodiya.a@opn.ooo>
2026-05-06 11:31:45 +0530

    Implement rediection flow for installment meethod

--- view/frontend/web/js/view/payment/omise.js
+++ view/frontend/web/js/view/payment/omise.js
@@ -8,14 +8,40 @@ define(
         rendererList
     ) {
         'use strict';
-        
-        const METHOD_RENDERERS = Object.keys(window.checkoutConfig.omise_payment_list);
 
+        function checkWlbStatus(omise_payment_list) {
+            const methods = omise_payment_list?.omise_offsite_installment || [];
+
+            return methods.some(method => 
+                method.name.startsWith('installment_wlb')
+            ) ? 1 : 0;
+        }
+
+        const omisePaymentList = window.checkoutConfig.omise_payment_list;
+        var is_wlb = checkWlbStatus(omisePaymentList);
+
+        const METHOD_RENDERERS = Object.keys(window.checkoutConfig.omise_payment_list);
+        const UPA_FEATURE = window.checkoutConfig.omise_upa_feature;
         METHOD_RENDERERS.forEach(rendererName => {
-            rendererList.push({
-                type: rendererName,
-                component: 'Omise_Payment/js/view/payment/method-renderer/' + rendererName.replace(/_/g, '-') + '-method'
-            });
+            if(rendererName == "omise_offsite_installment" && UPA_FEATURE){
+                if(!is_wlb){
+                    rendererList.push({
+                        type: rendererName,
+                        component: 'Omise_Payment/js/view/payment/method-renderer/omise-offsite-upa-installment' + '-method'
+                    });    
+                }else{
+                    rendererList.push({
+                        type: rendererName,
+                        component: 'Omise_Payment/js/view/payment/method-renderer/' + rendererName.replace(/_/g, '-') + '-method'
+                    });
+                }
+            }else{
+                rendererList.push({
+                    type: rendererName,
+                    component: 'Omise_Payment/js/view/payment/method-renderer/' + rendererName.replace(/_/g, '-') + '-method'
+                });
+            }
+            
         });
 
         rendererList.push(
